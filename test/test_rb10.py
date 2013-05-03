157654d8de682fb8a9f7652de59f6e8d6697e1d7
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-05-04 01:36:45 +0800

    test: enable tests for rb10 and pta09
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- test/test_rb10.py
+++ test/test_rb10.py
@@ -16,23 +16,23 @@ import dataMgr.whImporter as IMPORT
 #imp.partReimport('rb10', 'rb1010_dayk', '2009-10-16', '2010-10-15')
 #imp.partReimport('rb10', 'rb0910_dayk', '2009-3-27', '2009-10-15')
 
-#turt = TURT.Turt1('rb1310', 'rb1310_dayk', 'rb1310_dayk_trade_rec', 'history')
-#turt.setAttrs(3, 1, 40)
-#turt.run()
+turt = TURT.Turt1('rb1310', 'rb1310_dayk', 'rb1310_dayk_trade_rec', 'history')
+turt.setAttrs(3, 1, 40)
+turt.run()
 
-#turt.atr()
+turt.atr()
 
-#turt = TURT.Turt1('rb1210', 'rb1210_dayk', 'rb1210_dayk_trade_rec', 'history')
-#turt.setAttrs(3, 1, 40)
-#turt.run()
+turt = TURT.Turt1('rb1210', 'rb1210_dayk', 'rb1210_dayk_trade_rec', 'history')
+turt.setAttrs(3, 1, 40)
+turt.run()
 
-#turt = TURT.Turt1('rb1110', 'rb1110_dayk', 'rb1110_dayk_trade_rec', 'history')
-#turt.setAttrs(3, 1, 40)
-#turt.run()
+turt = TURT.Turt1('rb1110', 'rb1110_dayk', 'rb1110_dayk_trade_rec', 'history')
+turt.setAttrs(3, 1, 40)
+turt.run()
 
-#turt = TURT.Turt1('rb1010', 'rb1010_dayk', 'rb1010_dayk_trade_rec', 'history')
-#turt.setAttrs(3, 1, 40)
-#turt.run()
+turt = TURT.Turt1('rb1010', 'rb1010_dayk', 'rb1010_dayk_trade_rec', 'history')
+turt.setAttrs(3, 1, 40)
+turt.run()
 
 turt = TURT.Turt1('rb0910', 'rb0910_dayk', 'rb0910_dayk_trade_rec', 'history')
 turt.setAttrs(3, 1, 40)
