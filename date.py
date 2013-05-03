e9ebc8ff352343f2938f3caf03ba0ada788d075b
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-05-03 16:44:41 +0800

    date: add two methods and update code.
    1) Add getDateNextDays() to return the date by next number of days.
    2) Add getDatePrevDays() to return the date by previous number
       of days.
    3) Rename _formatRetDate() to _dateToString().
    4) Update all date comparison uising _dateToString().
    5) Add tests for getDateNextDays() and getDatePrevDays().
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- date.py
+++ date.py
@@ -24,31 +24,29 @@ class Date:
 		#print self.dateSet
 		return
 	
-	def _formatRetDate (self, date):
+	def _dateToString (self, date):
 		retDate = '%s' % date
 		return retDate
 	
 	def curDate (self):
 		if self.dateIndex >= 0 and self.dateIndex < self.indexBound:
-			return self._formatRetDate(self.dateSet[self.dateIndex][0])
+			return self._dateToString(self.dateSet[self.dateIndex][0])
 		return None
 	
 	def firstDate (self):
-		return self._formatRetDate(self.dateSet[0][0])
+		return self._dateToString(self.dateSet[0][0])
 	
 	def lastDate (self):
-		return self._formatRetDate(self.dateSet[self.indexBound-1][0])
+		return self._dateToString(self.dateSet[self.indexBound-1][0])
 	
 	def isFirstDate (self, date):
-		time = '%s' % (self.dateSet[0][0])
-		if time == date:
+		if self._dateToString(self.dateSet[0][0]) == date:
 			return True
 		else:
 			return False
 	
 	def isLastDate (self, date):
-		time = '%s' % (self.dateSet[self.indexBound-1][0])
-		if time == date:
+		if self._dateToString(self.dateSet[self.indexBound-1][0]) == date:
 			return True
 		else:
 			return False
@@ -56,10 +54,9 @@ class Date:
 	def nextDate (self, date):
 		i = 0
 		while i < self.indexBound:
-			time = '%s' % (self.dateSet[i][0])
-			if time == date:
+			if self._dateToString(self.dateSet[i][0]) == date:
 				if i+1 < self.indexBound:
-					return self._formatRetDate(self.dateSet[i+1][0])
+					return self._dateToString(self.dateSet[i+1][0])
 				else:
 					return None
 			i = i + 1
@@ -69,10 +66,9 @@ class Date:
 	def prevDate (self, date):
 		i = 0
 		while i < self.indexBound:
-			time = '%s' % (self.dateSet[i][0])
-			if time == date:
+			if self._dateToString(self.dateSet[i][0]) == date:
 				if i-1 >= 0:
-					return self._formatRetDate(self.dateSet[i-1][0])
+					return self._dateToString(self.dateSet[i-1][0])
 				else:
 					return None	
 			i = i + 1
@@ -85,8 +81,7 @@ class Date:
 		#print time
 		i = 0
 		while i < self.indexBound:
-			time = '%s' % (self.dateSet[i][0])
-			if time == date:
+			if self._dateToString(self.dateSet[i][0]) == date:
 				self.dateIndex = i
 				return i
 			i = i + 1
@@ -96,18 +91,46 @@ class Date:
 	def getSetNextDate (self):
 		if self.dateIndex + 1 < self.indexBound:
 			self.dateIndex = self.dateIndex + 1
-			return self._formatRetDate(self.dateSet[self.dateIndex][0])
+			return self._dateToString(self.dateSet[self.dateIndex][0])
 		else:
 			return None
 	
 	def getSetPrevDate (self):
 		if self.dateIndex - 1 >= 0:
 			self.dateIndex = self.dateIndex - 1
-			return self._formatRetDate(self.dateSet[self.dateIndex][0])
+			return self._dateToString(self.dateSet[self.dateIndex][0])
 		else:
 			return None
 	
-	def getDateNextDays (self, month, year):
-		
-		return
+	def getDateNextDays (self, date, days):
+		i = 0
+		retDate = None
+		while i < self.indexBound:
+			if self._dateToString(self.dateSet[i][0]) == date:
+				if i + days < self.indexBound:
+					retDate =  self._dateToString(self.dateSet[i+days][0])
+				break
+			elif self._dateToString(self.dateSet[i][0]) > date:
+				if i + days - 1 < self.indexBound:
+					retDate =  self._dateToString(self.dateSet[i + days - 1][0])
+				break
+			i += 1
+
+		return retDate
+			
+	def getDatePrevDays (self, date, days):
+		i = self.indexBound - 1
+		retDate = None
+		while i >= 0:
+			if self._dateToString(self.dateSet[i][0]) == date:
+				if i - days >= 0:
+					retDate =  self._dateToString(self.dateSet[i-days][0])
+				break
+			elif self._dateToString(self.dateSet[i][0]) < date:
+				if i - days + 1 >= 0:
+					retDate =  self._dateToString(self.dateSet[i - days + 1][0])
+				break
+			i -= 1
+
+		return retDate
 		
--- test/test_date.py
+++ test/test_date.py
@@ -7,13 +7,27 @@ def test_date():
 	"""
 	2012-05-31
 	2012-06-01
-	2012-06-01
 	2012-06-04
 	2012-06-05
 	2012-06-06
 	127
 	2012-12-04
+	2012-12-03
 	2012-12-05
+	None
+	2012-12-27
+	None
+	2012-06-01
+	True
+	False
+	True
+	False
+	2012-06-12 next 5 days: 2012-06-19
+	2012-06-22 next 3 days: 2012-06-27
+	2012-12-26 next 5 days: None
+	2012-06-12 prev 5 days: 2012-06-05
+	2012-06-22 prev 3 days: 2012-06-19
+	2012-05-31 prev 5 days: None
 	"""
 	time = date.Date('futures', 'm1305_day_k')
 	res = time.curDate()
@@ -57,6 +71,14 @@ def test_date():
 	print res
 	res = time.isLastDate('2012-05-31')
 	print res
+	
+	print '2012-06-12 next 5 days: %s' % time.getDateNextDays('2012-06-12', 5)
+	print '2012-06-22 next 3 days: %s' % time.getDateNextDays('2012-06-22', 3)
+	print '2012-12-26 next 5 days: %s' % time.getDateNextDays('2012-12-26', 5)
+	
+	print '2012-06-12 prev 5 days: %s' % time.getDatePrevDays('2012-06-12', 5)
+	print '2012-06-22 prev 3 days: %s' % time.getDatePrevDays('2012-06-22', 3)
+	print '2012-05-31 prev 5 days: %s' % time.getDatePrevDays('2012-05-31', 5)
 
 if __name__=='__main__':
 	test_date()
