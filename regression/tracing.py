c29873f63c734727259c9e11f24fcd851c0f2055
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-05-09 15:54:42 +0800

    regression:tracing: add support to trace more fields
    Add support to trace more fields, such as, Close, Open, Highest,
    Lowest.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- regression/tracing.py
+++ regression/tracing.py
@@ -9,25 +9,30 @@ database = 'history'
 #time = '2013-5-7'
 #days = 9
 
-def printBound (table, time, days):
+def printBound (table, time, days, field):
 	dat = data.Data(database, table)
-	print '\n%s lowest with %s days up to %s' % (table, days, time), dat.highestUpToDate(time, days)
-	print '%s highest with %s days up to %s' % (table, days, time), dat.lowestUpToDate(time, days)
+	print '\n%s highest with %s days up to %s' % (table, days, time), dat.highestUpToDate(time, days, field)
+	print '%s lowest with %s days up to %s' % (table, days, time), dat.lowestUpToDate(time, days, field)
 
 
-def showBound (time, days):
-	printBound('m1309_dayk', time, days)
-	printBound('sr1309_dayk', time, days)
-	printBound('rb1310_dayk', time, days)
-	printBound('pta1309_dayk', time, days)
+def showBound (time, days, field):
+	printBound('m1309_dayk', time, days, field)
+	printBound('sr1309_dayk', time, days, field)
+	printBound('rb1310_dayk', time, days, field)
+	printBound('pta1309_dayk', time, days, field)
 	
 	
 if __name__ == '__main__':
-	if len(sys.argv) != 3:
+	if len(sys.argv) < 3:
 		print '2 arguments are required!'
 		exit()
 	#print 	len(sys.argv)
+	
+	field = 'Close'
 	time = sys.argv[1]
 	days = int(sys.argv[2])
-	showBound (time, days)
+	if len(sys.argv) == 4:
+		field = sys.argv[3]
+	
+	showBound (time, days, field)
 
