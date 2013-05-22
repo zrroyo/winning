00f988e6369066101a3188146722e901b56de48e
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-05-22 16:43:37 +0800

    date: add comments for all methods
    Add comments for all methods.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- date.py
+++ date.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 
+'''
+This is the core framework to manage all records in Time field for a data table.
+
+Fetch all Time records from a data table at initialization, then directly use them 
+without accesssing database again and again, this brings convenience and improves 
+performance.
+'''
+
 import db.mysqldb as sql
 
 class Date:
@@ -13,6 +21,7 @@ class Date:
 		self.db.close()
 		return
 	
+	# Fill dateset using the records stored in Time field of a data table.
 	def fillDates (self, table):
 		sqls = 'select Time from %s order by %s asc' % (table, 'Time')
 		#self.db.search(table, None, 'Day')
@@ -24,33 +33,40 @@ class Date:
 		#print self.dateSet
 		return
 	
+	# Transfer a date value to a string.
 	def _dateToString (self, date):
 		retDate = '%s' % date
 		return retDate
 	
+	# Return the current date referred by current index in dateset.
 	def curDate (self):
 		if self.dateIndex >= 0 and self.dateIndex < self.indexBound:
 			return self._dateToString(self.dateSet[self.dateIndex][0])
 		return None
 	
+	# Return the first date in dateset.
 	def firstDate (self):
 		return self._dateToString(self.dateSet[0][0])
 	
+	# Return the last date in dateset.
 	def lastDate (self):
 		return self._dateToString(self.dateSet[self.indexBound-1][0])
 	
+	# Return if @date is the first date in dateset.
 	def isFirstDate (self, date):
 		if self._dateToString(self.dateSet[0][0]) == date:
 			return True
 		else:
 			return False
 	
+	# Return if @date is the last date in dateset.
 	def isLastDate (self, date):
 		if self._dateToString(self.dateSet[self.indexBound-1][0]) == date:
 			return True
 		else:
 			return False
 	
+	# Return the next date behind passed @date.
 	def nextDate (self, date):
 		i = 0
 		while i < self.indexBound:
@@ -63,6 +79,7 @@ class Date:
 
 		return None
 	
+	# Return the previous date before passed @date.
 	def prevDate (self, date):
 		i = 0
 		while i < self.indexBound:
@@ -74,7 +91,8 @@ class Date:
 			i = i + 1
 
 		return None
-
+		
+	# Set @date as current date.
 	def setCurDate (self, date):
 		#print date
 		#time = '%s' % (self.dateSet[0][0])
@@ -88,6 +106,7 @@ class Date:
 			
 		return None
 	
+	# Return current date and set next date as current date.
 	def getSetNextDate (self):
 		if self.dateIndex + 1 < self.indexBound:
 			self.dateIndex = self.dateIndex + 1
@@ -95,6 +114,7 @@ class Date:
 		else:
 			return None
 	
+	# Return current date and set previous date as current date.
 	def getSetPrevDate (self):
 		if self.dateIndex - 1 >= 0:
 			self.dateIndex = self.dateIndex - 1
@@ -102,6 +122,7 @@ class Date:
 		else:
 			return None
 	
+	# Get the date behind @date by @days days.
 	def getDateNextDays (self, date, days):
 		i = 0
 		retDate = None
@@ -118,6 +139,7 @@ class Date:
 
 		return retDate
 			
+	# Get the date before @date by @days days.
 	def getDatePrevDays (self, date, days):
 		i = self.indexBound - 1
 		retDate = None
