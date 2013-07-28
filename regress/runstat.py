eaca2552e294d51710814d0679354d4e2ae3a08d
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-07-28 23:29:58 +0800

    regress:runstat: add Market Run-time Statistics Block
    Add Market Run-time Statistics Block which helps to do
    runtime statistics counting for a market.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- regress/runstat.py
+++ regress/runstat.py
@@ -4,8 +4,11 @@
 RunStat: runtime statistics.
 '''
 
+import thread
+
+# Run-time Statistics Block. only used for a single future code.
 class RunStat:
-	def __init__ (self, name):
+	def __init__ (self, name=None):
 		self.name = name	# The name for which do statistics.
 		self.maxWin = 0		# The max profit won in a business.
 		self.maxLoss = 0	# The max loss lost in a business.
@@ -84,3 +87,87 @@ class RunStat:
 		self._formatPrint("       Total Profit", self.profit)
 		print "		* * * * * * * * * * * * * \n"
 	
+# Market Run-time Statistics Block. Used for a whole market.
+class MarketRunStat(RunStat):
+	def __init__ (self, maxAllowedPos):
+		RunStat.__init__(self)
+		self.curFutCode = None			# The future code (name).
+		self.maxAllowedPos = maxAllowedPos	# The max allowed actions to add positions in a market.
+		self.curPoses = 0			# Current positions in a market.
+		self.lock = thread.allocate_lock()	# The lock to protect the varibles in object.
+		self.busProfit = 0			# The total profit earned in a bussiness for a market (if
+							# self.curPoses decreases to 0).
+		return
+	
+	def __exit__ (self):
+		return
+	
+	# Decide whether allowed to open a position.
+	def openPosition (self):
+		self.lock.acquire()
+		if self.curPoses >= self.maxAllowedPos:
+			self.lock.release()
+			return False
+		
+		self.curPoses += 1
+		self.lock.release()
+		return True	
+	
+	# Decide whether allowed to close a position.
+	def closePosition (self):
+		self.lock.acquire()
+		if self.curPoses == 0:
+			self.lock.release()
+			return False
+		
+		self.curPoses -= 1
+		self.lock.release()
+		return True
+	
+	# Update all counted attributes.
+	def update (self, profit):
+		self.lock.acquire()
+		self.updateProfit(profit)
+		self.lock.release()
+			
+	# Update business profit.
+	def updateBusinessProfit (self, profit):
+		# Update Max Profit.
+		self.lock.acquire()
+		if self.curPoses != 0:
+			self.busProfit += profit
+			self.lock.release()
+			return
+		
+		if self.busProfit + profit > self.maxBusProfit:
+			self.maxBusProfit = self.busProfit + profit
+			
+		# Update Min Profit.
+		if self.busProfit + profit < self.minBusProfit:
+			self.minBusProfit = self.busProfit + profit
+			
+		self.busProfit = 0
+		self.lock.release()
+			
+	# Format the print.
+	def _formatPrint (self, comment, value):
+		print "	  %s:	%s" % (comment, value)
+		
+	# Show all market run statistics.
+	def showMarRunStat (self):
+		self.lock.acquire()
+		if self.curPoses != 0:
+			self.lock.release()
+			return
+		
+		print "\n	* * * * * * * * * * * * * "
+		print "	Market Run Time Statistics:"
+		self._formatPrint("Max Business Profit", self.maxBusProfit)
+		self._formatPrint("Min Business Profit", self.minBusProfit)
+		self._formatPrint("         Max Profit", self.maxProfit)
+		self._formatPrint("         Min Profit", self.minProfit)
+		self._formatPrint("       Total Profit", self.profit)
+		#self._formatPrint("  Current Positions", self.curPoses)
+		print "	* * * * * * * * * * * * * \n"
+		
+		self.lock.release()
