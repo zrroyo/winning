9b82a7af78a1c2d31a1baa70e6dcb0b0669b158d
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-07-28 23:27:39 +0800

    regress:tick: add commentaries and new methods
    1) Add setCurTick() and curTickBehind().
    2) Add commentaries for methods.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- regress/tick.py
+++ regress/tick.py
@@ -8,7 +8,7 @@ system needs to make decision if need to do any operation.
 Ruan Zhengwang (ruan.zhengwang@gmail.com)
 '''
 
-#import time
+import time
 
 class Tick:
 	def __init__ (self, year=1990, month=1, day=1):
@@ -54,27 +54,40 @@ class Tick:
 		
 		return maxDay
 		
+	# Timetick format.	
 	def tickString (self, year, month, day):
 		return '%s-%s-%s' % (year, month, day)
 	
+	# Get the time clock value for @tick
 	def tickTime (self, tick):
 		return time.strptime('%s' % tick, '%Y-%m-%d')
 	
+	# Get the year value in @tick
+	def tickYear (self, tick):
+		sep = tick.split('-')
+		return int('%s' % sep[0])
+		
+	# Get the month value in @tick
+	def tickMonth (self, tick):
+		sep = tick.split('-')
+		return int('%s' % sep[1])
+	
+	# Get the day value in @tick
+	def tickDay (self, tick):
+		sep = tick.split('-')
+		return int('%s' % sep[2])
+	
+	# Get current tick.
 	def curTick (self):
 		return self.tickString(self.year, self.month, self.day)
 	
-	def tickYear (self, tick):
-		sep = tick.split('-')
-		return '%s' % sep[0]
+	# Set current tick with @tick.
+	def setCurTick (self, tick):
+		self.year = self.tickYear(tick)
+		self.month = self.tickMonth(tick)
+		self.day = self.tickDay(tick)
 		
-	def tickMonth (self, tick):
-		sep = tick.split('-')
-		return '%s' % sep[1]
-	
-	def tickDay (self, tick):
-		sep = tick.split('-')
-		return '%s' % sep[2]
-		
+	# Set and get next tick.
 	def tickNext (self):
 		year = self.year
 		month = self.month
@@ -91,7 +104,17 @@ class Tick:
 		self.month = month
 		self.day = day
 		newTick = self.tickString(year, month, day)
+		#print 'Tick Src: New tick %s' % newTick
 		return newTick
+		
+	# Judge if self.tick is leg behind a time tick.
+	def curTickBehind (self, tick):
+		t1 = time.strptime(self.curTick(), '%Y-%m-%d')
+		t2 = time.strptime(tick, '%Y-%m-%d')
+		
+		#print self.curTick(), tick, t1 < t2
+		
+		return t1 < t2
 		
 #if __name__ == '__main__':
 	#tick = Tick(2013, 1, 1)
