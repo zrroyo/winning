84272089a2b7930a2b1a2d7fa828b51ed408dfce
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-08-17 18:22:20 +0800

    ctp: add linux api so
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

---
+++ ctp/testctp.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+
+#from TraderApi import TraderApi, TraderSpi
+#import UserApiStruct as usturct
+
+import thosttraderapi
+
+#THOST_TERT_RESTART  = 0
+#THOST_TERT_RESUME   = 1
+#THOST_TERT_QUICK    = 2
+
+def doTest():
+	#tApi = TraderApi.CreateTraderApi("trader")
+	#tSpi = TraderSpi()
+	#tApi.RegisterSpi(tSpi)
+	#tApi.SubscribePublicTopic(THOST_TERT_QUICK)
+	#tApi.SubscribePrivateTopic(THOST_TERT_QUICK)
+	#tApi.RegisterFront("tcp://172.16.0.31:26205")
+	#tApi.Init()
+	
+	return
+
+if __name__ == '__main__':
+	doTest()
