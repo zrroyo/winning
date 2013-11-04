69e8a83b6c1ddc8b228d01e02ed2137cef91c866
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-11-04 10:37:09 +0800

    ctp:testctp: update server/ports, etc
    Update server and port using scqh.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- ctp/testctp.py
+++ ctp/testctp.py
@@ -19,12 +19,12 @@ def testMdApi():
 	#inst=[u'm1401', u'p1401']
 	inst=[u'rb1401', u'm1401']
 	#inst=[u'm1401']
-	#mdSpi = CtpMdApi(inst, '2030', '00092', '888888', None)
+	#mdSpi = CtpMdApi(inst, '1024', '00092', '888888', None)
 	
 	agent = ctpagent.MarketDataAgent()
-	mdSpi = CtpMdApi(inst, '2030', '00092', '888888', agent)
+	mdSpi = CtpMdApi(inst, '1024', '00000038', '123456', agent)
 	mdSpi.Create("Md")
-	mdSpi.RegisterFront('tcp://asp-sim2-md1.financial-trading-platform.com:26213')
+	mdSpi.RegisterFront('tcp://180.166.30.117:41213')
 	mdSpi.Init()
 
 	while 1:
@@ -37,9 +37,9 @@ def testTraderApi():
 	#inst=[u'm1401', u'p1401']
 	inst=[u'rb1401', u'm1401']
 	#inst=[u'm1401']
-	traderSpi = CtpTraderApi(inst, broker_id="2030",investor_id="wang.yc",passwd="123456")
+	traderSpi = CtpTraderApi(inst, broker_id="1024",investor_id="00000038",passwd="123456")
 	traderSpi.Create("trader")
-	traderSpi.RegisterFront('tcp://asp-sim2-front1.financial-trading-platform.com:26205')
+	traderSpi.RegisterFront('tcp://180.166.30.117:41205')
 	traderSpi.Init()
 	
 	while 1:
