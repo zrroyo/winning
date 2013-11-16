4ef534b4a255c06c832483c5abf2efc6117ffd7f
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-11-16 19:52:03 +0800

    dataMgr:data: rewrite methods inherited from Data for CtpData
    1) Remove M40() and add M30() for Data.
    2) Add moving average methods for CtpData.
    3) Add lowestUpToDate() and highestUpToDate() for CtpData.
    
    Merged zw-dev 8840e2e5.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- dataMgr/data.py
+++ dataMgr/data.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append("..")
 import time
 import db.mysqldb as sql
-from ctp.ctpagent import MarketDataAgent
+#from ctp.ctpagent import MarketDataAgent
 
 #数据库数据对象
 class Data:
@@ -57,8 +57,8 @@ class Data:
 	def M20 (self, date, field='Close'):
 		return self.M(date, field, 20)
 	
-	def M40 (self, date, field='Close'):
-		return self.M(date, field, 40)
+	def M30 (self, date, field='Close'):
+		return self.M(date, field, 30)
 	
 	def M60 (self, date, field='Close'):
 		return self.M(date, field, 60)
@@ -131,9 +131,11 @@ class CtpData(Data):
 		mdagent		#行情数据代理
 		):
 		Data.__init__(self, database, table)
+		self.instrument = instrument
 		self.workDay = workDay
 		self.mdlocal = mdagent.mdlocal
 		
+	#判断是否是已过去的交易日
 	def __isPastDate (self, date):
 		t1 = time.strptime(date, '%Y-%m-%d')
 		t2 = time.strptime(self.workDay, '%Y-%m-%d')
@@ -146,29 +148,127 @@ class CtpData(Data):
 			print 'CtpData: Bad date'
 			return False
 			
+	#计算当前交易日的移动均线值
 	def ctp_M (self, date, field='Close', days=1):
+		'''
+		CTP的当前时间必然要大于数据库中所存储的最大时间，
+		数据库中存储的为过去的数据，而CTP中的为当前数据。
+		'''
+		cond = 'Time < "%s" order by Time desc limit %d' % (date, days-1)
+		#print cond
+		num = self.db.search(self.table, cond, field)
+		res = self.db.fetch('all')
+		#print res
+		#print res[0][0]
+		
+		if field == 'Close':
+			value = self.mdlocal.getClose(self.instrument)
+		elif field == 'Open':
+			value = self.mdlocal.getOpen(self.instrument)
+		elif field == 'Highest':
+			value = self.mdlocal.getHighest(self.instrument)
+		elif field == 'Lowest':
+			value = self.mdlocal.getLowest(self.instrument)
+				
+		return (self.sum(res) + value) / (num + 1)
+		
+	#5日移动均线
+	def M5 (self, date, field='Close'):
+		if self.__isPastDate(date):
+			return self.M(date, field, 5)
+		else:
+			return self.ctp_M(date, field, 5)
+			
+	#10日移动均线
+	def M10 (self, date, field='Close'):
+		#print self.__isPastDate(date)
+		if self.__isPastDate(date):
+			return self.M(date, field, 10)
+		else:
+			return self.ctp_M(date, field, 10)
 	
+	#20日移动均线
+	def M20 (self, date, field='Close'):
+		if self.__isPastDate(date):
+			return self.M(date, field, 20)
+		else:
+			return self.ctp_M(date, field, 20)
+			
+	#30日移动均线
+	def M30 (self, date, field='Close'):
+		if self.__isPastDate(date):
+			return self.M(date, field, 30)
+		else:
+			return self.ctp_M(date, field, 30)
+			
+	#60日移动均线
+	def M60 (self, date, field='Close'):
+		if self.__isPastDate(date):
+			return self.M(date, field, 60)
+		else:
+			return self.ctp_M(date, field, 60)
+			
+	#得到开盘价
 	def getOpen (self, date):
 		if self.__isPastDate(date):
 			return self.M(date, 'Open', 1)
 		else:
 			return self.mdlocal.getOpen(self.instrument)
 		
+	#得到收盘价
 	def getClose (self, date):
 		if self.__isPastDate(date):
 			return self.M(date, 'Close', 1)
 		else:
 			return self.mdlocal.getClose(self.instrument)
 			
+	#得到最高价
 	def getHighest (self, date):
 		if self.__isPastDate(date):
 			return self.M(date, 'Highest', 1)
 		else:
 			return self.mdlocal.getHighest(self.instrument)
 			
+	#得到最低价
 	def getLowest (self, date):
 		if self.__isPastDate(date):
 			return self.M(date, 'Lowest', 1)
 		else:
 			return self.mdlocal.getLowest(self.instrument)
-				
+			
+	#得到包含当前交易日的数天内的最低价
+	def lowestUpToDate (self, date, days, field='Close'):
+		if self.__isPastDate(date):
+			return Data.lowestUpToDate(self, date, days, field)
+		
+		lowest = Data.lowestBeforeDate(self, date, days, field)
+		
+		if field == 'Close':
+			value = self.mdlocal.getClose(self.instrument)
+		elif field == 'Open':
+			value = self.mdlocal.getOpen(self.instrument)
+		elif field == 'Highest':
+			value = self.mdlocal.getHighest(self.instrument)
+		elif field == 'Lowest':
+			value = self.mdlocal.getLowest(self.instrument)
+			
+		return min(lowest, value)
+		
+	#得到包含当前交易日的数天内的最高价
+	def highestUpToDate (self, date, days, field='Close'):
+		if self.__isPastDate(date):
+			return Data.highestUpToDate(self, date, days, field)
+		
+		highest = Data.highestUpToDate(self, date, days, field)
+		
+		if field == 'Close':
+			value = self.mdlocal.getClose(self.instrument)
+		elif field == 'Open':
+			value = self.mdlocal.getOpen(self.instrument)
+		elif field == 'Highest':
+			value = self.mdlocal.getHighest(self.instrument)
+		elif field == 'Lowest':
+			value = self.mdlocal.getLowest(self.instrument)
+			
+		return max(highest, value)
+			
