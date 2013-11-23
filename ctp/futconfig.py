f060ebcb3a31b1314dbd6f6d3e9507aa925c7c0b
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-11-23 17:10:44 +0800

    ctp:futconfig: add CtpConfig and new methods
    1) Add CtpConfig class to access ctp configurations.
    2) Rename FutureConfig to TradingConfig.
    3) Add getEnabled() and getDatabase() methods for TradingConfig.
    4) Add test for #1 and #3.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- ctp/futconfig.py
+++ ctp/futconfig.py
@@ -11,12 +11,12 @@ sys.path.append('..')
 
 from misc.genconfig import GenConfig
 
-class FutureConfig(GenConfig):
+#交易（策略）配置文件读取接口
+class TradingConfig(GenConfig):
 	def getInstruments (self, section):
 		return self.getSecOption(section, 'instruments')
 		
 	def getStrategy (self, section):
-		
 		return self.getSecOption(section, 'strategy')
 		
 	def getAllowedPos(self, section):
@@ -43,16 +43,51 @@ class FutureConfig(GenConfig):
 	def getNeedConfirm (self, section):
 		return self.getSecOption(section, 'need_confirm')
 		
+	def getEnabled (self, section):
+		return self.getSecOption(section, 'enabled')
+		
+	def getDatabase (self, section):
+		return self.getSecOption(section, 'database')
+		
+#CTP服务器端口配置文件读取接口
+class CtpConfig(GenConfig):
+	def getServer (self, section):
+		return self.getSecOption(section, 'server')	
+		
+	def getPasswd (self, section):
+		return self.getSecOption(section, 'passwd')
+		
+	def getInvestor (self, section):
+		return self.getSecOption(section, 'investor')
+		
+	def getBrokerid (self, section):
+		return self.getSecOption(section, 'broker_id')
+		
 		
 if __name__ == '__main__':
-	futCfg = FutureConfig('../config/test.ini')
-	print futCfg.getInstruments('m')
-	print futCfg.getStrategy('m')
-	print futCfg.getAllowedPos('m')
-	print futCfg.getMaxAddPos('m')
-	print futCfg.getMinPos('m')
-	print futCfg.getMinPosIntv('m')
-	print futCfg.getPriceUnit('m')
-	print futCfg.getStartDate('m')
-	print futCfg.getTimeCtpOn('m')
-	print futCfg.getNeedConfirm('m')
+	print u'测试TradingConfig接口：'
+	tradeConfig = TradingConfig('../config/test.ini')
+	print tradeConfig.getInstruments('m')
+	print tradeConfig.getStrategy('m')
+	print tradeConfig.getAllowedPos('m')
+	print tradeConfig.getMaxAddPos('m')
+	print tradeConfig.getMinPos('m')
+	print tradeConfig.getMinPosIntv('m')
+	print tradeConfig.getPriceUnit('m')
+	print tradeConfig.getStartDate('m')
+	print tradeConfig.getTimeCtpOn('m')
+	print tradeConfig.getNeedConfirm('m')
+	print tradeConfig.getEnabled('m')
+	print tradeConfig.getDatabase('m')
+
+	print u'\n测试CtpConfig接口：'
+	ctpConfig = CtpConfig('../config/ctp.ini')
+	print ctpConfig.getServer('MarketData')
+	print ctpConfig.getPasswd('MarketData')
+	print ctpConfig.getInvestor('MarketData')
+	print ctpConfig.getBrokerid('MarketData')
+	print ctpConfig.getServer('Trade')
+	print ctpConfig.getPasswd('Trade')
+	print ctpConfig.getInvestor('Trade')
+	print ctpConfig.getBrokerid('Trade')
+	
