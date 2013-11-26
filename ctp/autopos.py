6710551c1eba4d5c9e23c52ec0cac63790f58fff
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-11-26 23:38:49 +0800

    ctp:autopos: add log method to manage output
    1) Add log() method to manage output uniformly.
    2) Support to use the log interface of a strategy.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- ctp/autopos.py
+++ ctp/autopos.py
@@ -28,19 +28,24 @@ class CtpAutoPosition:
 	timeWaitOrder = 10	#等待某操作完成时限(默认值)
 	
 	def __init__ (self, 
-		mdAgent, 	#行情数据代理
-		tdAgent		#交易服务器端代理
+		mdAgent,	#行情数据代理
+		tdAgent,	#交易服务器端代理
+		strategy=None	#交易策略对象（实例）
 		):
 		self.mdAgent = mdAgent
 		self.tdAgent = tdAgent
 		self.mdlocal = mdAgent.mdlocal
+		'''
+		有的时候需要访问在被调用策略里的一些接口、变量。
+		'''
+		self.strategy = strategy
 		
 	#撤消下单
 	def cancel_order (self, instrument, order_ref):
 		try:
 			self.tdAgent.cancel_command(instrument, order_ref)
 		except:	
-			print 'CtpPosition: cancel_order error'
+			self.log('CtpPosition: cancel_order error')
 			
 	#开仓
 	def open_position (self, instrument, direction, price, volume):
@@ -56,23 +61,25 @@ class CtpAutoPosition:
 				#如果开仓不成功，刚重新获取市场价并报单重开
 				self.cancel_order(instrument, order_ref)
 				price = self.mdlocal.getClose(instrument)
-				print 'Cancel order %d, new price %d' % (order_ref, price)
+				self.log('Cancel order %d, new price %d' % (order_ref, price))
 				order_ref = self.tdAgent.open_position(instrument, direction, price, volume)
 				ret = price
 				sleep(self.timeWaitOrder)
 				
 			return price
 		except:
-			print 'CtpPosition: open_position error'
+			self.log('CtpPosition: open_position error')
 			return None
 		
 		
 	#开多
 	def open_long_position (self, instrument, price, volume):
+		self.log('Opening [long] position  %g, %d' % (price, volume))
 		return self.open_position(instrument, D_Buy, price, volume)
 	
 	#开空
 	def open_short_position (self, instrument, price, volume):
+		self.log('Opening [short] position  %g, %d' % (price, volume))
 		return self.open_position(instrument, D_Sell, price, volume)
 		
 	#平仓
@@ -86,21 +93,35 @@ class CtpAutoPosition:
 				#如果平仓不成功，刚重新获取市场价并报单重平
 				self.cancel_order(instrument, order_ref)
 				price = self.mdlocal.getClose(instrument)
-				print 'Cancel order %d, new price %d' % (order_ref, price)
+				self.log('Cancel order %d, new price %d' % (order_ref, price))
 				order_ref = self.tdAgent.close_position(instrument, direction, price, volume, cos_flag)
 				ret = price
 				sleep(self.timeWaitOrder)
 				
 			return price
 		except:
-			print 'CtpPosition: close_position error'
+			self.log('CtpPosition: close_position error')
 			return None
 		
 	#平多
 	def close_long_position (self, instrument, price, volume, cos_flag=OF_Close):
+		self.log('Closing [long] position %g, %d' % (price, volume))
 		return self.close_position(instrument, D_Sell, price, volume, cos_flag)
 	
 	#平空
 	def close_short_position (self, instrument, price, volume, cos_flag=OF_Close):
+		self.log('Closing [short] position  %g, %d' % (price, volume))
 		return self.close_position(instrument, D_Buy, price, volume, cos_flag)
+		
+	#日志（输出）统一接口
+	def log (self, logMsg, *args):
+		'''
+		运用被调用策略的日志输出接口统一输出，避免混乱。
+		'''
+		logs = logMsg % (args)
+		if self.strategy is not None:
+			#print logMsg, args
+			self.strategy.log(logs)
+		else:
+			print logs
 		
