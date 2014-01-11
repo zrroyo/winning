0f40598e92c40ee950d33f40ae0bfa440f9dcd1d
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2014-01-11 22:10:54 +0800

    regress:emulate: fix priority for emulation threads not taking effect
    The priorities for emulation threads are not taking effect, fix it.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- regress/emulate.py
+++ regress/emulate.py
@@ -136,6 +136,8 @@ class Emulate:
 						futCode = self.futList.pop()
 						self.ctrlSet.setApplied(i)
 						#print '# %s priority %d, priMgr list %s' % (futCode, self.ctrlSet.priMgr.updatePriority(i), self.ctrlSet.priMgr.listPriorities())
+						self.ctrlSet.priMgr.updatePriority(i)
+						self.ctrlSet.priMgr.listPriorities()
 						self.ctrlSet.releaseLock(i)
 						
 						#print 'Thread %d:' % i
