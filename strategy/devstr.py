de4c0c35525e0a81b6d12caa8ddaefff187a1b8e
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2014-04-17 22:04:31 +0800

    dev1: add devstr module
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

---
+++ strategy/devstr.py
@@ -0,0 +1,85 @@
+#-*- coding:utf-8 -*-
+
+from turt1 import *
+
+#
+class DevStr (Turt1):
+	def __init__ (self, futName, dataTable, tradeTable, database='futures', runStat=None):
+		Turt1.__init__(self, futName, dataTable, tradeTable, database, runStat)
+	
+	#做空
+	def doShort (self, 
+		dateSet,	#时间集
+		date,		#开始日期
+		):
+		days = 0
+		self.emptyPostion()
+		pLastAddPrice = self.openShortPosition(self.data.getClose(date))
+		time = self.moveToNextTick(dateSet,
+				price = self.data.getClose(date),
+				direction = 'short')
+	
+		minPosIntv = self.minPosIntv
+
+		while time is not None:
+			days = days + 1
+			price = self.data.getClose(time)
+			
+			if self.stableHighestInPastDays(price, time, 10):
+				price = self.closeAllPostion(price, 'short')
+				self.log("	[Short] [%s] Hit Highest in 10 days: Clear all! %d days:	open %s,  close %s, highest %d" % (time, days, self.data.getClose(date), price, self.data.highestBeforeDate(time, 10)))
+				#time = dateSet.getSetNextDate()
+				break
+				
+			if self.stableToAddShortPosition(time, pLastAddPrice, price, minPosIntv):
+				if self.curPostion() < self.maxAddPos:
+					price = self.openShortPosition(price)
+					if price is not None:
+						self.log("	[Short] [%s] add postion	last add %s,  close %s, intv %d" % (time, pLastAddPrice, price, pLastAddPrice-price))
+						pLastAddPrice = price
+				
+			time = self.moveToNextTick(dateSet, 
+						price = self.data.getClose(time), 
+						direction = 'short'
+						)
+				
+		return time
+		
+	#做多
+	def doLong (self, 
+		dateSet,	#时间集
+		date,		#开始日期
+		):
+		days = 0
+		self.emptyPostion()
+		pLastAddPrice = self.openLongPosition(self.data.getClose(date))
+		time = self.moveToNextTick(dateSet,
+				price = self.data.getClose(date),
+				direction = 'long')
+			
+		minPosIntv = self.minPosIntv
+		
+		while time is not None:
+			days = days + 1
+			price = self.data.getClose(time)
+			
+			if self.stableLowestInPastDays(price, time, 10):
+				price = self.closeAllPostion(price, 'long')
+				self.log("	[Long] [%s] Hit Lowest in 10 days: Clear all! %d days:	open %s,  close %s, lowest %d" % (time, days, self.data.getClose(date), price, self.data.lowestBeforeDate(time, 10)))
+				#time = dateSet.getSetNextDate()
+				break
+			
+			if self.stableToAddLongPosition(time, pLastAddPrice, price, minPosIntv):
+				if self.curPostion() < self.maxAddPos:
+					price = self.openLongPosition(price)
+					if price is not None:
+						self.log("	[Long] [%s] add postion	last add %s,  close %s, intv %d" % (time, pLastAddPrice, price, price-pLastAddPrice))
+						pLastAddPrice = price
+						
+			time = self.moveToNextTick(dateSet, 
+						price = self.data.getClose(time), 
+						direction = 'long'
+						)
+				
+		return time
+	
