8c03243b0de6c2d936e3b09efd57bc220686dca1
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2014-05-11 23:23:47 +0800

    misc:profit: replace all old debug references with the new Debug interface
    There are many similar debug calls defined in classes, so simply replace them
    with the new universal Debug interface.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- misc/profit.py
+++ misc/profit.py
@@ -7,6 +7,8 @@
 
 import thread
 
+from debug import *
+
 '''
 利润及回撤类
 '''
@@ -15,22 +17,15 @@ class ProfitRegress:
 		debug = False,		#调试模式
 		numInstruments = 2,	#并行执行的合约数量
 		):
+		self.debug = Debug('ProfitRegress', debug)	#调试接口
 		self.profit = 0		#当前利润
 		self.maxProfit = 0	#盈利最高点
 		self.minProfit = 0	#盈利最低点
-		self.debug = debug
 		self.numInstruments = numInstruments
 		
 		self.lock = thread.allocate_lock()	#保护锁，保护整个数据结构
 		self.tmpProfit = 0	#临时利润累积
 		self.countTimes = 0	#已统计的合约数
-		
-	#打印调试信息
-	def dbg (self,
-		dbgInfo,	#debug信息
-		):
-		if self.debug:
-			print '	ProfitRegress: %s' % dbgInfo
 		
 	#更新最大最小利润
 	def updateMaxMinProfit (self, 
@@ -46,7 +41,7 @@ class ProfitRegress:
 		
 		self.tmpProfit += profit
 		newProfit = self.profit + self.tmpProfit
-		self.dbg('Time %s, newProfit %s, profit1 %s, profit2 %s, profit %s' % 
+		self.debug.dbg('Time %s, newProfit %s, profit1 %s, profit2 %s, profit %s' % 
 				(time, newProfit, self.tmpProfit - profit, profit, self.profit))
 		self.tmpProfit = 0
 		self.countTimes = 0
@@ -55,12 +50,12 @@ class ProfitRegress:
 		#更新盈利最高点
 		if newProfit > self.maxProfit:
 			self.maxProfit = newProfit
-			self.dbg('Time %s, Max profit %s' % (time, self.maxProfit))
+			self.debug.dbg('Time %s, Max profit %s' % (time, self.maxProfit))
 			
 		#更新盈利最低点
 		if newProfit < self.minProfit:
 			self.minProfit = newProfit
-			self.dbg('Time %s, Min profit %s' % (time, self.minProfit))
+			self.debug.dbg('Time %s, Min profit %s' % (time, self.minProfit))
 			
 	#累积报单利润
 	def addProfit (self, 
@@ -76,18 +71,11 @@ class BusinessWin:
 	def __init__ (self,
 		debug = False,	#调试模式
 		):
-		self.debug = debug
+		self.debug = Debug('BusinessWin', debug)	#调试接口
 		self.lock = thread.allocate_lock()	#保护锁
 		self.busWin = 0				#交易赢利
 		self.maxBusinessWin = 0			#最高盈利
 		self.minBusinessWin = 0			#最低盈利
-	
-	#打印调试信息
-	def dbg (self,
-		dbgInfo,	#debug信息
-		):
-		if self.debug:
-			print '	BusinessWin: %s' % dbgInfo
 	
 	#累积利润
 	def addBusWin (self,
@@ -124,18 +112,11 @@ class OrderWin:
 	def __init__ (self,
 		debug = False,	#调试模式
 		):
-		self.debug = debug
+		self.debug = Debug('OrderWin', debug)	#调试接口
 		self.lock = thread.allocate_lock()	#保护锁
 		self.maxOrderWin = 0			#最大盈利单
 		self.maxOrderLoss = 0			#最大止损单
-		
-	#打印调试信息
-	def dbg (self,
-		dbgInfo,	#debug信息
-		):
-		if self.debug:
-			print '	OrderWin: %s' % dbgInfo
-			
+	
 	#更新最大报单赢利和亏损
 	def updateOrderMaxWinLoss (self, 
 		profit,	#利润
