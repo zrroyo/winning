e00d2d24a7a31487c8b655d061910914edbb75ff
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2015-05-10 23:25:08 +0800

    dataMgr:importer: remove useless code and bug fix
    1. Remove a section of useless code.
    2. Fix a bug that endTime must be given while importing a new table.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- dataMgr/importer.py
+++ dataMgr/importer.py
@@ -75,21 +75,25 @@ class Import:
 		self.__prepareImport(table, template)
 		
 		#如过滤间期有效则只导入指定区间的数据
-		if timeFilters is not None:
-			startTime,endTime = timeFilters.split(',')
+		if timeFilters:
+			tf = timeFilters.split(',')
+			startTime = tf[0]
+			endTime = None
+			if len(tf) == 2:
+				endTime = tf[1]
 		
 		for line in fileinput.input(file):
 			time,open,highest,lowest,close,avg,sellVol,buyVol = self.fileRecordToColumns(line)
 			
-			if timeFilters is not None:
+			if timeFilters:
 				timeRecord = strToDatetime(time, self.strTimeFormat())
 				#忽略所有早于开始日期的数据
-				if timeRecord < strToDatetime(startTime, self.strTimeFormat()):
+				if startTime and timeRecord < strToDatetime(startTime, self.strTimeFormat()):
 					#self.debug.dbg('Ignore %s' % time)
 					continue
 				
 				#已到截止日期，操作完成
-				if timeRecord > strToDatetime(endTime, self.strTimeFormat()):
+				if endTime and timeRecord > strToDatetime(endTime, self.strTimeFormat()):
 					#self.debug.dbg('Up to the end date %s' % endTime)
 					fileinput.close()
 					return
@@ -114,7 +118,7 @@ class Import:
 			time,open,highest,lowest,close,avg,sellVol,buyVol = self.fileRecordToColumns(line)
 			
 			#忽略所有大于endTime的数据，并结束
-			if endTime is not None and strToDatetime(time, self.strTimeFormat()) > strToDatetime(endTime, self.strTimeFormat()):
+			if endTime and strToDatetime(time, self.strTimeFormat()) > strToDatetime(endTime, self.strTimeFormat()):
 				self.debug.dbg('Appended all data until %s' % endTime)
 				fileinput.close()
 				return
@@ -160,141 +164,8 @@ class Import:
 		
 		#print 'partReimportTo'
 		return res
-		
-	# Get the value from the field specifed by $field from a $record in which 
-	# each field is separated by space.
-	def getRecordFieldSepBySpace (self, record, field=1):
-		cmdStr = 'echo "%s" | awk \'{print $%d}\' ' % (record, field)
-		res = os.popen(cmdStr.strip())
-		#print res.read().strip()
-		#print res
-		return res.read().strip()
 	
-	# Get the value from the field specifed by $field from a $record in which 
-	# each field is separated by comma.
-	def getRecordFieldSepByComma (self, record, field=1):
-		cmdStr = 'echo "%s" | awk \'BEGIN{FS=","}END{print $%d}\' ' % (record, field)
-		res = os.popen(cmdStr.strip())
-		#print res.read().strip()
-		#print res
-		return res.read().strip()
-	
-	# Return the max date in month.
-	def _maxDateInMonth (self, month, Year):
-		if month == 1:
-			maxDate = '01-31'
-		elif month == 2:
-			maxDate = '02-29'
-		elif month == 3:
-			maxDate = '03-31'
-		elif month == 4:
-			maxDate = '04-30'
-		elif month == 5:
-			maxDate = '05-31'
-		elif month == 6:
-			maxDate = '06-30'
-		elif month == 7:
-			maxDate = '07-31'
-		elif month == 8:
-			maxDate = '08-31'
-		elif month == 9:
-			maxDate = '09-30'
-		elif month == 10:
-			maxDate = '10-31'
-		elif month == 11:
-			maxDate = '11-30'
-		elif month == 12:
-			maxDate = '12-31'
-		else:
-			return None
-		
-		date = '%s-' % Year
-		date += maxDate
-		return date
-	
-	# Decide the start date for a sub future determinded by year and 
-	# month in a future table.
-	def _startDateSubFuture (self, dateSet, year, month, endDays):
-		if month > 12 or month < 1:
-			return None
-		
-		if month == 1:
-			month = 12
-			year -= 1
-		else:
-			month -= 1
-		
-		maxDatePrevMonth = self._maxDateInMonth(month, year-1)
-		
-		#print maxDatePrevMonth
-		if maxDatePrevMonth <= dateSet.firstDate():
-			return dateSet.firstDate()
-		
-		return dateSet.getDateNextDays(maxDatePrevMonth, endDays+1)
-		
-	# Decide the end date for a sub future determinded by year and 
-	# month in a future table.
-	def _endDateSubFuture (self, dateSet, year, month, endDays):
-		if month > 12 or month < 1:
-			return None
-		
-		if month == 1:
-			month = 12
-			year -= 1
-		else:
-			month -= 1
-					
-		maxDatePrevMonth = self._maxDateInMonth(month, year)
-		
-		#print maxDatePrevMonth
-		if maxDatePrevMonth >= dateSet.lastDate():
-			return dateSet.lastDate()
-		
-		return dateSet.getDateNextDays(maxDatePrevMonth, endDays)
-	
-	# Make the future name with the future code, year and month.
-	def _tableNameTemplate (self, futCode, year, month):
-		if month > 12 or month < 1:
-			return None
-		
-		year = year%2000
-		if year < 10:
-			year = '0%s' % year
-			
-		if month < 10:
-			month = '0%s' % month
-			
-		name = '%s%s%s' % (futCode, year, month)
-		#print name
-		return name
-	
-	# Split a big future table which in consisted of many sub-future (future by year) tables 
-	# into sub-futures tables.	
-	def splitTableToSubFutures (self, futCode, dataTable, month, endDays, yearStart, yearEnd=0):
-		if self.db.ifTableExist(dataTable) == False:
-			print '\n	Table "%s" does not exist!\n' % dataTable
-			return
-		
-		if month > 12 or month < 1:
-			print '\n	Month "%s" is out of range [1~12]\n' % month
-			return
-		
-		lcDateSet = Date(self.database, dataTable)
-		year = yearStart
-		
-		while year >= yearEnd:
-			startDate = self._startDateSubFuture(lcDateSet, year, month, endDays)
-			endDate = self._endDateSubFuture(lcDateSet, year, month, endDays)
-			tableName = self._tableNameTemplate(futCode, year, month)
-			tableName += '_dayk'
-			print tableName, startDate, endDate
-			self.partReimport(dataTable, tableName, startDate, endDate)
-			
-			if startDate == lcDateSet.firstDate():
-				break
-			
-			year -= 1
-	
-	def dropFutureTable (self, dataTable):
-		return self.db.drop(dataTable)
+	#删掉数据表
+	def dropDataTable (self, table):
+		return self.db.drop(table)
 	
