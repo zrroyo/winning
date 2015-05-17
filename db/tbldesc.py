ce3eb7e401aaa256e17fee660231662a76c9e37c
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2015-05-17 18:50:15 +0800

    db: introduce tbldesc module
    Introduce tbldesc to describe the table structure in database.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

---
+++ db/tbldesc.py
@@ -0,0 +1,32 @@
+#-*- coding:utf-8 -*-
+
+'''
+Author: Zhengwang Ruan <ruan.zhengwang@gmail.com>
+Start: 2015年 05月 17日 星期日 17:23:49 CST
+
+数据表描述
+'''
+
+#数据表字段(Field)
+F_TIME		= 'Time'
+F_OPEN		= 'Open'
+F_HIGH		= 'High'
+F_LOW		= 'Low'
+F_CLOSE		= 'Close'
+F_AVG		= 'Avg'
+F_VOLUME	= 'Volume'
+F_OPENINTEREST	= 'OpenInterest'
+F_TR		= 'Tr'
+F_ATR		= 'Atr'
+
+#数据表查询完成后返回的列表中的索引号(Field Number)
+FN_TIME		= 0
+FN_OPEN		= 1
+FN_HIGH		= 2
+FN_LOW		= 3
+FN_CLOSE	= 4
+FN_AVG		= 5
+FN_VOLUME	= 6
+FN_OPENINTEREST	= 7
+FN_TR		= 8
+FN_ATR		= 9
