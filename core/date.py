c7ec55466aa407d8e92332b4814e7653e72bd3a9
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2015-05-24 13:29:40 +0800

    core:date: introduce Ticks class
    Date is ambiguous to some degree, so introduce Ticks class, which is actually an
    alias of Date, to replace Date from now on.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- core/date.py
+++ core/date.py
@@ -7,20 +7,25 @@ Author: Zhengwang Ruan <ruan.zhengwang@gmail.com>
 交易时间管理模块
 '''
 
+import sys
+sys.path.append('..')
+
 from db.sql import *
 from db.tbldesc import *
 from misc.debug import *
 
 #交易时间管理接口
 class Date:
-	
-	debug = Debug('Date', False)	#调试接口
-	
-	def __init__ (self, database, table):
+	def __init__ (self, 
+		database, 	#数据库
+		table,		#数据表
+		debug = False,	#是否调试
+		):
 		self.db = SQL()
 		self.db.connect(database)
 		self.fillDates(table)
 		self.table = table
+		self.debug = Debug('Date', debug)	#调试接口
 		self.current = self.firstDate()
 	
 	def __del__ (self):
@@ -106,7 +111,7 @@ class Date:
 		else:
 			return True
 	
-	#返回传入交易时间后的第某个交易时间
+	#返回传入交易时间后的第@limit个交易时间
 	def getNexNumDate (self, 
 		date,	#交易时间
 		limit,	#限制数目
@@ -129,7 +134,7 @@ class Date:
 		else:
 			return None
 	
-	#返回传入交易时间前的第某个交易时间
+	#返回传入交易时间前的第@limit个交易时间
 	def getPrevNumDate (self, 
 		date,	#交易时间
 		limit,	#限制数目
@@ -151,35 +156,115 @@ class Date:
 			return self.__dateToString(time)
 		else:
 			return None
+		
+	#返回交易时间在数据表中的索引号（位置）
+	def getDateIndex (self, 
+		date,	#交易时间
+		):
+		strSql = "select count(%s) from %s where %s <= '%s' order by %s desc" % (
+				F_TIME, self.table, F_TIME, date, F_TIME)
+		self.debug.dbg("getDateIndex strSql: %s" % strSql)
+		self.db.execSql(strSql)
+		res = self.db.fetch(0)
+		return res[0]
+	
+#Tick(交易时间)管理接口
+class Ticks(Date):
+	#返回当前Tick
+	def curTick (self):
+		return self.curDate()
+	
+	#返回数据表里的第一个Tick
+	def firstTick (self):
+		return self.firstDate()
+
+	#返回数据表里的最后一个Tick
+	def lastTick (self):
+		return self.lastDate()
+	
+	#是否是第一个Tick
+	def isFirstTick (self, 
+		tick,	#Tick
+		):
+		return self.isFirstDate(tick)
+	
+	#是否是最后一个Tick
+	def isLastTick (self, 
+		tick,	#Tick
+		):
+		return self.isLastDate(tick)
+	
+	#返回当前Tick后的下一个Tick
+	def nextTick (self):
+		return self.nextDate()
+	
+	#返回当前Tick前的上一个Tick
+	def prevTick (self):
+		return self.prevDate()
+	
+	#设置当前Tick
+	def setCurTick (self, 
+		tick,	#Tick
+		):
+		return self.setCurDate(tick)
+	
+	#设置下一Tick为当前Tick，并返回
+	def getSetNextTick (self):
+		return self.getSetNextDate()
+	
+	#设置上一Tick为当前Tick，并返回
+	def getSetPrevTick (self):
+		return self.getSetPrevDate()
+	
+	#返回传入Tick后的第@limit个Tick
+	def getNexNumTick (self, 
+		tick,	#Tick
+		limit,	#限制数目
+		):
+		return self.getNexNumDate(tick, limit)
+
+	#返回传入Tick前的第@limit个Tick
+	def getPrevNumTick (self, 
+		tick,	#Tick
+		limit,	#限制数目
+		):
+		return self.getPrevNumDate(tick, limit)
+	
+	#返回交易时间在数据表中的索引号（位置）
+	def getTickIndex (self, 
+		tick,	#交易时间
+		):
+		return self.getDateIndex(tick)
 	
 #测试
 def doTest ():
-	date = Date('history2', 'p1601_mink')
-	print "First Date %s" % date.firstDate()
-	print "Last Date %s" % date.lastDate()
+	tick = Ticks('history2', 'p1601_mink', debug = False)
+	print "First Tick %s" % tick.firstTick()
+	print "Last Tick %s" % tick.lastTick()
 	time = '2015-01-19 10:56:00'
-	date.setCurDate(time)
-	print "Current Time %s" % date.curDate()
-	print "Prev Date %s" % date.prevDate()
-	print "Next Date %s" % date.nextDate()
-	date.getSetNextDate()
-	print "Current Time %s" % date.curDate()
-	date.getSetPrevDate()
-	print "Current Time %s" % date.curDate()
-	print date.getPrevNumDate(time, 10)
-	print date.getNexNumDate(time, 10)
-	print date.isFirstDate("2015-01-16 21:00:00")
-	print date.isLastDate('2015-05-08 11:29:00')
-	print date.isFirstDate('2015-05-09 11:29:00')
-	print date.isLastDate('2015-05-09 11:29:00')
-	
+	tick.setCurTick(time)
+	print "Current Time %s" % tick.curTick()
+	print "Prev Tick %s" % tick.prevTick()
+	print "Next Tick %s" % tick.nextTick()
+	tick.getSetNextTick()
+	print "Current Time %s" % tick.curTick()
+	tick.getSetPrevTick()
+	print "Current Time %s" % tick.curTick()
+	print tick.getPrevNumTick(time, 10)
+	print tick.getNexNumTick(time, 10)
+	print tick.isFirstTick("2015-01-16 21:00:00")
+	print tick.isLastTick('2015-05-08 11:29:00')
+	print tick.isFirstTick('2015-05-09 11:29:00')
+	print tick.isLastTick('2015-05-09 11:29:00')
+	print tick.getTickIndex(time)
+
 	#测试结果
 	'''
-	First Date 2015-01-16 21:00:00
-	Last Date 2015-05-08 11:29:00
+	First Tick 2015-01-16 21:00:00
+	Last Tick 2015-05-08 11:29:00
 	Current Time 2015-01-19 10:56:00
-	Prev Date 2015-01-19 10:53:00
-	Next Date 2015-01-19 11:01:00
+	Prev Tick 2015-01-19 10:53:00
+	Next Tick 2015-01-19 11:01:00
 	Current Time 2015-01-19 11:01:00
 	Current Time 2015-01-19 10:56:00
 	2015-01-19 09:08:00
@@ -188,6 +273,7 @@ def doTest ():
 	True
 	False
 	False
+	16
 	'''
 
 if __name__ == '__main__':
