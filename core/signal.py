bcd13fa1ab676e326b6d50cdcbfc64edcd32358f
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2015-05-25 23:09:51 +0800

    core: introduce signal module
    Introduce signal module to define the trading signals.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

---
+++ core/signal.py
@@ -0,0 +1,12 @@
+#-*- coding:utf-8 -*-
+
+'''
+Author: Zhengwang Ruan <ruan.zhengwang@gmail.com>
+Start: 2015年 05月 24日 星期日 17:55:55 CST
+
+信号模块
+'''
+
+#期货信号
+SIG_TRADE_LONG		= 0
+SIG_TRADE_SHORT		= 1
