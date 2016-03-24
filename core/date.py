39e6e2b8bf57623408d1edba610623adcb13dd74
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2016-03-24 14:55:27 +0800

    core:date: re-implement to improve efficiency
    1. Re-implement to improve efficiency by loading all ticks to memory initially.
    2. Code cleanup.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- core/date.py
+++ core/date.py
@@ -1,248 +1,264 @@
 #! /usr/bin/python
 #-*- coding:utf-8 -*-
 
-'''
+"""
 Author: Zhengwang Ruan <ruan.zhengwang@gmail.com>
 
 交易时间管理模块
-'''
+"""
 
 import sys
 sys.path.append('..')
+from datetime import datetime
 
 from db.sql import *
 from db.tbldesc import *
 from misc.debug import *
 
-#交易时间管理接口
+# 交易时间管理接口
 class Date:
 	def __init__ (self, 
 		database, 	#数据库
 		table,		#数据表
 		debug = False,	#是否调试
 		):
+		self.debug = Debug('Date', debug)	#调试接口
 		self.db = SQL()
 		self.db.connect(database)
-		self.fillDates(table)
 		self.table = table
-		self.debug = Debug('Date', debug)	#调试接口
-		self.__firstDate = None	#第一个tick
-		self.__lastDate = None	#最后一个tick
-		self.current = self.firstDate()
-	
+		# 存放ticks的缓冲区
+		self.ticksBuffer = []
+		self.numTicks = 0
+		self.current = 0
+		self.__loadTicks()
+
 	def __del__ (self):
 		self.db.close()
-	
+
+	# 从数据库中加载时间
+	def __loadTicks (self):
+		strSql = 'select %s from %s order by %s asc' % (
+				F_TIME, self.table, F_TIME)
+		self.db.execSql(strSql)
+		# time = self.db.fetch(all)[FN_TIME]
+		self.ticksBuffer = [ e[0] for e in list(self.db.fetch('all'))]
+		# self.debug.dbg(self.ticksBuffer)
+		self.numTicks = len(self.ticksBuffer)
+		self.current = 0
+		# self.debug.dbg("__loadTicks: ticks buffer %s, num %s" % (
+		# 		self.ticksBuffer, self.numTicks))
+
 	#无意义，仅用于往后兼容性支持
 	def fillDates (self, 
 		table,
 		):
-		return
-	
-	#把数据库时间转换成字符串
-	def __dateToString (self, 
+		pass
+
+	# 把数据库时间转换成字符串
+	def __dateToString (self,
 		date,	#数据库时间
 		):
-		return '%s' % date
-	
+		return datetime.strftime(date, "%Y-%m-%d %H:%M:%S")
+
+	# 把时间字符串转换成数据库时间
+	def __strToDateTime (self,
+		strDate,
+		):
+		return datetime.strptime(strDate, "%Y-%m-%d %H:%M:%S")
+
 	#返回当前交易时间
 	def curDate (self):
-		return self.current
+		try:
+			return self.ticksBuffer[self.current]
+		except IndexError, e:
+			self.debug.error("curDate: error: %s" % e)
+			return None
 	
-	#返回数据表里的第一个交易时间
+	# 返回数据表里的第一个交易时间
 	def firstDate (self):
-		if self.__firstDate is not None:
-			return self.__firstDate
-		
-		strSql = 'select %s from %s order by %s asc limit 1' % (
-				F_TIME, self.table, F_TIME)
-		self.db.execSql(strSql)
-		time = self.db.fetch(0)[FN_TIME]
-		self.debug.dbg("firstDate %s" % time)
-		self.__firstDate = self.__dateToString(time)
-		return self.__firstDate
+		try:
+			return self.ticksBuffer[0]
+		except IndexError, e:
+			self.debug.error("firstDate: error: %s" % e)
+			return None
 	
-	#返回数据表里的最后一个交易时间
+	# 返回数据表里的最后一个交易时间
 	def lastDate (self):
-		if self.__lastDate is not None:
-			return self.__lastDate
-		
-		strSql = "select %s from %s order by %s desc limit 1" % (
-				F_TIME, self.table, F_TIME)
-		self.db.execSql(strSql)
-		time = self.db.fetch(0)[FN_TIME]
-		self.debug.dbg("lastDate %s" % time)
-		self.__lastDate = self.__dateToString(time)
-		return self.__lastDate
-	
-	#是否是第一个交易时间
+		try:
+			return self.ticksBuffer[self.numTicks - 1]
+		except IndexError, e:
+			self.debug.error("firstDate: error: %s" % e)
+			return None
+
+	# 是否是第一个交易时间
 	def isFirstDate (self, 
 		date,	#交易时间
 		):
+		if isinstance(date, str):
+			date = self.__strToDateTime(date)
+
 		return True if date == self.firstDate() else False
 	
-	#是否是最后一个交易时间
+	# 是否是最后一个交易时间
 	def isLastDate (self, 
 		date,	#交易时间
 		):
+		if isinstance(date, str):
+			date = self.__strToDateTime(date)
+
 		return True if date == self.lastDate() else False
 	
-	#返回当前交易时间后的下一个交易时间
+	# 返回当前交易时间后的下一个交易时间
 	def nextDate (self):
-		return self.getNexNumDate(self.current, 1)
-	
-	#返回当前交易时间前的上一个交易时间
+		try:
+			ret = self.ticksBuffer[self.current + 1]
+			self.debug.dbg("current %s, ret %s " % (self.current, ret))
+			return ret
+		except IndexError, e:
+			self.debug.error("nextDate: error: %s" % e)
+			return None
+
+	# 返回当前交易时间前的上一个交易时间
 	def prevDate (self):
-		return self.getPrevNumDate(self.current, 1)
-	
-	#设置当前交易时间
+		try:
+			return self.ticksBuffer[self.current - 1]
+		except IndexError, e:
+			self.debug.error("prevDate: error: %s" % e)
+			return None
+
+	# 设置当前交易时间
 	def setCurDate (self, 
 		date,	#交易时间
 		):
-		#不做检验真的好吗？？
-		self.current = date
+		try:
+			if isinstance(date, str):
+				date = self.__strToDateTime(date)
+
+			idx = self.ticksBuffer.index(date)
+			self.current = idx
+		except ValueError, e:
+			self.debug.error("setCurDate: error: %s" % e)
 	
-	#设置下一交易时间为当前交易时间，并返回
+	# 设置下一交易时间为当前交易时间，并返回
 	def getSetNextDate (self):
-		self.current = self.nextDate()
-		return self.current
-	
-	#设置上一交易时间为当前交易时间，并返回
+		ret = self.nextDate()
+		if ret:
+			self.current += 1
+
+		return ret
+
+	# 设置上一交易时间为当前交易时间，并返回
 	def getSetPrevDate (self):
-		self.current = self.prevDate()
-		return self.current
-	
-	#检验交易时间是否有效
-	def __validDate (self,
-		date,	#交易时间
-		):
-		if date is None:
-			return False
-		else:
-			return True
-	
-	#返回传入交易时间后的第@limit个交易时间
+		ret = self.prevDate()
+		if ret:
+			self.current -= 1
+
+		return ret
+
+	# 返回指定交易时间之后的第@limit个交易时间
 	def getNexNumDate (self, 
 		date,	#交易时间
 		limit,	#限制数目
 		):
-		strSql = "select %s from %s where %s > '%s' order by %s asc limit %s" % (
-				F_TIME, self.table, F_TIME, date, F_TIME, limit)
-		self.db.execSql(strSql)
-		res = self.db.fetch('all')
-		
-		#只有返回数量够数才表明查找成功
-		if len(res) == limit:
-			time = res[limit-1][FN_TIME]
-		else:
-			time = None
-		
-		self.debug.dbg("getNexNumDate %s" % time)
-		
-		if self.__validDate(time):
-			return self.__dateToString(time)
-		else:
+		try:
+			if isinstance(date, str):
+				date = self.__strToDateTime(date)
+
+			idx = self.ticksBuffer.index(date)
+			idx += limit
+			ret =  self.ticksBuffer[idx]
+			# self.debug.dbg("getNexNumDate: limit %s, %s" % (limit, ret))
+			return ret
+
+		except (ValueError, IndexError), e:
+			self.debug.error("getNexNumDate: limit %s, error: %s" % (limit, e))
 			return None
-	
-	#返回传入交易时间前的第@limit个交易时间
+
+	# 返回指定交易时间之前的第@limit个交易时间
 	def getPrevNumDate (self, 
 		date,	#交易时间
 		limit,	#限制数目
 		):
-		strSql = "select %s from %s where %s < '%s' order by %s desc limit %s" % (
-				F_TIME, self.table, F_TIME, date, F_TIME, limit)
-		self.db.execSql(strSql)
-		res = self.db.fetch('all')
+		return self.getNexNumDate(date, -limit)
 		
-		#只有返回数量够数才表明查找成功
-		if len(res) == limit:
-			time = res[limit-1][FN_TIME]
-		else:
-			time = None
-		
-		self.debug.dbg("getPrevNumDate %s" % time)
-		
-		if self.__validDate(time):
-			return self.__dateToString(time)
-		else:
-			return None
-		
-	#返回交易时间在数据表中的索引号（位置）
+	# 返回交易时间在数据表中的索引号（位置）
 	def getDateIndex (self, 
 		date,	#交易时间
 		):
-		strSql = "select count(%s) from %s where %s <= '%s' order by %s desc" % (
-				F_TIME, self.table, F_TIME, date, F_TIME)
-		self.debug.dbg("getDateIndex strSql: %s" % strSql)
-		self.db.execSql(strSql)
-		res = self.db.fetch(0)
-		return res[0]
-	
-#Tick(交易时间)管理接口
+		try:
+			if isinstance(date, str):
+				date = self.__strToDateTime(date)
+
+			return self.ticksBuffer.index(date)
+		except ValueError, e:
+			self.debug.error("getDateIndex: error: %s" % e)
+			return None
+
+# Tick(交易时间)管理接口
 class Ticks(Date):
-	#返回当前Tick
+	# 返回当前Tick
 	def curTick (self):
 		return self.curDate()
 	
-	#返回数据表里的第一个Tick
+	# 返回数据表里的第一个Tick
 	def firstTick (self):
 		return self.firstDate()
 
-	#返回数据表里的最后一个Tick
+	# 返回数据表里的最后一个Tick
 	def lastTick (self):
 		return self.lastDate()
 	
-	#是否是第一个Tick
+	# 是否是第一个Tick
 	def isFirstTick (self, 
-		tick,	#Tick
+		tick,
 		):
 		return self.isFirstDate(tick)
 	
-	#是否是最后一个Tick
+	# 是否是最后一个Tick
 	def isLastTick (self, 
-		tick,	#Tick
+		tick,
 		):
 		return self.isLastDate(tick)
 	
-	#返回当前Tick后的下一个Tick
+	# 返回当前Tick后的下一个Tick
 	def nextTick (self):
 		return self.nextDate()
 	
-	#返回当前Tick前的上一个Tick
+	# 返回当前Tick前的上一个Tick
 	def prevTick (self):
 		return self.prevDate()
 	
-	#设置当前Tick
+	# 设置当前Tick
 	def setCurTick (self, 
-		tick,	#Tick
+		tick,
 		):
 		return self.setCurDate(tick)
 	
-	#设置下一Tick为当前Tick，并返回
+	# 设置下一Tick为当前Tick，并返回
 	def getSetNextTick (self):
 		return self.getSetNextDate()
 	
-	#设置上一Tick为当前Tick，并返回
+	# 设置上一Tick为当前Tick，并返回
 	def getSetPrevTick (self):
 		return self.getSetPrevDate()
 	
-	#返回传入Tick后的第@limit个Tick
+	# 返回传入Tick后的第@limit个Tick
 	def getNexNumTick (self, 
 		tick,	#Tick
 		limit,	#限制数目
 		):
 		return self.getNexNumDate(tick, limit)
 
-	#返回传入Tick前的第@limit个Tick
+	# 返回传入Tick前的第@limit个Tick
 	def getPrevNumTick (self, 
 		tick,	#Tick
 		limit,	#限制数目
 		):
 		return self.getPrevNumDate(tick, limit)
 	
-	#返回交易时间在数据表中的索引号（位置）
+	# 返回交易时间在数据表中的索引号（位置）
 	def getTickIndex (self, 
-		tick,	#交易时间
+		tick,
 		):
 		return self.getDateIndex(tick)
 
@@ -310,11 +326,17 @@ def doTest ():
 	True
 	False
 	False
-	16
+	15
 	%Y-%m-%d %H-%M-%S
 	%Y-%m-%d
 	'''
 
+#
+def doTest2 ():
+	# tickSrc = Ticks("history", "p1405_dayk", True)
+	tickSrc = Ticks('history2', 'p1601_mink', debug = True)
+
+
 if __name__ == '__main__':
 	doTest()
-	
+	# doTest2()
