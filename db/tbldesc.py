60d1069309b475eaaf352e197b2192955d7d24a7
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2016-04-09 19:17:22 +0800

    db:tbldesc: support to convert field name to field number
    Add a mapping to support to convert the field name to the field number.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- db/tbldesc.py
+++ db/tbldesc.py
@@ -31,6 +31,20 @@ FN_OPENINTEREST	= 7
 FN_TR		= 8
 FN_ATR		= 9
 
+# 字段名称到字段号映射表
+DATA_TBL_F_FN_MAP = {
+	F_TIME		: FN_TIME,
+	F_OPEN		: FN_OPEN,
+	F_HIGH		: FN_HIGH,
+	F_LOW		: FN_LOW,
+	F_CLOSE		: FN_CLOSE,
+	F_AVG		: FN_AVG,
+	F_VOLUME	: FN_VOLUME,
+	F_OPENINTEREST	: FN_OPENINTEREST,
+	F_TR		: FN_TR,
+	F_ATR		: FN_ATR
+}
+
 #交易单统计（Order Stat Dump）表字段
 OSD_F_ID		= 'DumpID'
 OSD_F_CONTRACT		= 'Contract'
