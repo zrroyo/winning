91997627aad1b3abcf15ed66b2ebda3530464b25
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2016-12-28 15:18:10 +0800

    misc:dateTime: support to convert datetime string to seconds
    1. support to convert datetime string to seconds since 1970
    2. Code cleanup.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- misc/dateTime.py
+++ misc/dateTime.py
@@ -1,36 +1,52 @@
 #! /usr/bin/python
 #-*- coding:utf-8 -*-
 
-'''
+"""
 时间处理模块
-'''
+"""
 
+import time
 from datetime import datetime
 
-#把时间转换成指定的datetime格式
-def strToDatetime (
-	timeStr,				#时间字符串
-	timeFormat = "%Y:%m:%d:%H:%M:%S",	#时间格式,如 "%H:%M:%S"
-	):
+def strToDatetime (timeStr, timeFormat = "%Y:%m:%d:%H:%M:%S"):
+	"""
+	把字符时间转换成指定的datetime格式
+	:param timeStr: 时间字符串
+	:param timeFormat: 时间格式,如 "%H:%M:%S"
+	:return: datetime时间
+	"""
 	try:
 		return datetime.strptime(timeStr, timeFormat)
-	except:
+	except Exception:
 		return None
 		
-#返回以指定datetime格式的当前时间
-def nowDatetime (
-	timeFormat = "%Y:%m:%d:%H:%M:%S", #时间格式,如 "%H:%M:%S"
-	):
+def nowDatetime (timeFormat = "%Y:%m:%d:%H:%M:%S"):
+	"""
+	返回当前datetime时间
+	:param timeFormat: 时间格式,如 "%H:%M:%S"
+	:return: datetime时间
+	"""
 	strTimeNow = datetime.now().strftime(timeFormat)
 	return datetime.strptime(strTimeNow, timeFormat)
 	
-#把datetime时间转换成字符串
-def datetimeToStr (
-	ptime,					#datetime格式时间
-	timeFormat = "%Y:%m:%d:%H:%M:%S",	#时间格式,如 "%H:%M:%S"
-	):
+def datetimeToStr (ptime, timeFormat = "%Y:%m:%d:%H:%M:%S"):
+	"""
+	把datetime时间转换成时间字符串
+	:param ptime: datetime格式时间
+	:param timeFormat: 时间格式,如 "%H:%M:%S"
+	:return: 时间字符串
+	"""
 	return datetime.strftime(ptime, timeFormat)
-	
+
+def mkTimeInSeconds (timeStr, timeFormat = "%Y:%m:%d:%H:%M:%S"):
+	"""
+	将时间字符串转化为time.time（）时间
+	:param timeStr: 时间字符串
+	:param timeFormat: 时间格式,如 "%H:%M:%S"
+	:return: 1979年以来秒数时间
+	"""
+	return time.mktime(datetime.strptime(timeStr, timeFormat).timetuple())
+
 #测试
 if __name__ == '__main__':
 	strTime = '2014:3:7:18:08:09'
@@ -42,4 +58,5 @@ if __name__ == '__main__':
 	print nowDatetime("%H:%M:%S")
 	print strToDatetime("18:08:09", "%H:%M:%S")
 	print datetimeToStr(nowDatetime(), "%d/%m/%Y")
+	print mkTimeInSeconds(strTime, "%Y:%m:%d:%H:%M:%S")
 	
