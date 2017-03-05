8634d66aee4e1722fce477fa59ece4a256a289db
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2017-03-05 13:16:36 +0800

    core:attribute: adjust default values and add margin ratio
    1. Adjustment for the default values.
    2. Support margin ratio.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- core/attribute.py
+++ core/attribute.py
@@ -9,26 +9,23 @@ Start: 2015年 05月 22日 星期五 23:50:13 CST
 
 #期货合约（交易）属性类
 class Attribute:
-	def __init__ (self,
-		maxPosAllowed = None,	#最大允许持仓单位数
-		numPosToAdd = None,	#每加仓单位所代表的手数
-		priceVariation = None,	#触发加仓条件的价格差
-		multiplier = None,	#合约乘数
-		):
-		self.maxPosAllowed = maxPosAllowed
-		self.numPosToAdd = numPosToAdd
-		self.priceVariation = priceVariation
-		self.multiplier = multiplier
-	
+	def __init__ (self):
+		self.maxPosAllowed = 0
+		self.numPosToAdd = 0
+		self.priceVariation = 0
+		self.multiplier = 0
+		self.marginRatio = 0.1
+
 	#设置属性
 	def set (self,
 		maxPosAllowed,	#最大允许持仓单位数
 		numPosToAdd,	#每加仓单位所代表的手数
 		priceVariation,	#触发加仓条件的价格差
 		multiplier,	#合约乘数
+		marginRatio,	#保证金比率
 		):
 		self.maxPosAllowed = maxPosAllowed
 		self.numPosToAdd = numPosToAdd
 		self.priceVariation = priceVariation
 		self.multiplier = multiplier
-	
+		self.marginRatio = marginRatio
