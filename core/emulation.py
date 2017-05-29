4e09935936f63eb6d1708db812b1609249c2e8c4
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2017-05-29 17:08:37 +0800

    core: introduce tbase module
    Introduce TBase class as the parent of Emulation, so the common support could be
    shared by other types of tests.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- core/emulation.py
+++ core/emulation.py
@@ -14,21 +14,14 @@ sys.path.append("..")
 import time
 import ctypes
 import Queue
-import multiprocessing as mp
 import pandas as pd
-from datetime import datetime
+import multiprocessing as mp
 
 from misc.debug import Debug
 from misc.utils import quickInsert
-from misc.dateTime import mkTimeInSeconds
+from tbase import *
 from corecfg import EmulationConfig, ContractDescConfig
 
-# 默认配置文件目录
-DEF_EMUL_CONFIG_DIR = "tests"
-# 默认合约描述文件
-DEF_CONTRACT_DESC_CFG = "config/contracts_desc"
-# 默认执行日志保存路径
-DEF_TEST_OUT_DIR = "TESTDATA"
 
 # 合约调度状态
 EMUL_SCHED_MODE_NSP		= 1
@@ -204,10 +197,6 @@ class ControlArea(ctypes.Structure):
 		    ('pos', ctypes.c_uint32),		#仓位，配合TK_STAT命令返回参数
 		    ('capital', ctypes.c_double)]	#资金，配合TK_STAT命令返回参数
 
-# 发现未识别策略异常
-class ExceptionEmulUnknown(Exception):
-	pass
-
 # 合约运行状态缓存数据结构（列表）中各部分数据对应的索引。
 PS_MP	= 0		#MP控制块
 PS_LOCK	= 1		#共享内存保护锁
@@ -215,7 +204,7 @@ PS_CTRL	= 2		#共享（内存）控制块
 PS_PRI	= 3		#优先级
 PS_CONT	= 4		#合约名
 
-class Emulation:
+class Emulation(TBase):
 	def __init__ (self, cfg, strategy, debug = False, storeLog = False):
 		"""
 		模拟交易
@@ -224,23 +213,9 @@ class Emulation:
 		:param debug: 是否调试
 		:param storeLog: 保存合约日志
 		"""
+		TBase.__init__(self, cfg, strategy, debug, storeLog)
 		self.debug = Debug('Emulation', debug)	#调试接口
-		self.dbgMode = debug
-		self.storeLog = storeLog
-		#发现未识别策略，提示调用函数处理
-		if strategy not in Emulation.validStrategy():
-			raise ExceptionEmulUnknown("Found unknown strategy.")
-		self.strategy = strategy
 
-		#
-		self.testCfg = "%s/%s" % (DEF_EMUL_CONFIG_DIR, cfg)
-		self.emuCfg = EmulationConfig(self.testCfg)
-		self.contracts = self.emuCfg.getContracts().strip(',').split(',')
-		self.startTicks = self.emuCfg.getStartTime().strip(',').split(',')
-		self.expireDates = self.emuCfg.getExpireDates().strip(',')
-		self.paraLevel = int(self.emuCfg.getParallelLevel())
-		# 初始化合约描述接口
-		self.descCfg = ContractDescConfig(DEF_CONTRACT_DESC_CFG)
 		# sched请求队列
 		self.msgQ = mp.Queue()
 		# sched请求管理接口
@@ -252,44 +227,6 @@ class Emulation:
 				    	capital = int(self.emuCfg.getParallelCapital()), debug = True)
 		# 优先级初始值
 		self.priority = 0
-		# 日志保存目录
-		self.logDir = None
-
-	# 所有支持的策略列表
-	@staticmethod
-	def validStrategy ():
-		ret = ['testfuture']
-		return ret
-
-	def __estimateEndTick (self, contract, expireDates):
-		"""
-		估计合约结束tick
-		:param contract: 合约名称
-		:param expireDates: 结束日期列表
-		:return: 合约结束tick
-		"""
-		date_in_contract = filter(str.isdigit, contract)
-		year = int(date_in_contract[0:2])
-		# 合约的实际结束日期应为交割月的上月
-		month = int(date_in_contract[2:4])
-		if month - 1 == 0:
-			year -= 1
-
-		# expireL = expireDates.split(',')
-		# 将所有合约到期时间生成以月份为key的字典以加速查找
-		# expireMap = dict([(int(ep.split('-')[0]), ep) for ep in expireL])
-		expireL = [ep.split(':') for ep in expireDates.split(',')]
-		expireMap = dict([(int(ep[0]), ep[1]) for ep in expireL])
-		ep = expireMap[month]
-
-		if year < 10:
-			ret = "200%s-%s" % (year, ep)
-		else:
-			ret = "20%s-%s" % (year, ep)
-
-		self.debug.dbg("date_in_contract %s, expire map %s, year %s, month %s, ret %s" % (
-					date_in_contract, expireMap, year, month, ret))
-		return ret
 
 	def __priorityAssigner (self):
 		"""
@@ -309,24 +246,14 @@ class Emulation:
 		:param follow: 跳过startTick，从下一tick开始
 		:return: None
 		"""
-		strategy = None
-		if self.strategy == "testfuture":
-			import strategy.test_dev
-			strategy = strategy.test_dev.TestFuture(contract = contract,
-						config = self.descCfg,
-						logDir = self.logDir,
-						debug = self.dbgMode)
-
-		strategy.setAttrs(maxPosAllowed = int(self.emuCfg.getContractAddMaxAllowed()),
-				numPosToAdd = int(self.emuCfg.getContractVolumeAdd()),
-				priceVariation = int(self.emuCfg.getContractTriggerLevel()))
+		strategy = self.getInstance(contract)
 
 		# 启动合约准备，分配共享（内存）控制块、保护锁
 		lock = mp.Lock()
 		ctrl = mp.Array(ControlArea, [(EMUL_SCHED_MODE_NSP, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)])
 		# self.debug.error("ctrl.mode: %s" % ctrl[0].mode)
 		p = mp.Process(target = strategy.start, args = (startTick,
-					self.__estimateEndTick(contract, expireDates),
+					self.estimateEndTick(contract, expireDates),
 					self.msgQ, ctrl[0], lock, self.storeLog, follow))
 		p.name = contract
 		p.start()
@@ -606,31 +533,7 @@ class Emulation:
 			if act == 0 and not self.__handleReqs():
 				break
 
-	def __initTestEnv(self, argv, name = None):
-		"""
-		初始化模拟测试环境
-		:param argv: 命令参数列表
-		:param name: 执行别名
-		:return: 成功返回True，否则返回False
-		"""
-		# 创建测试目录名
-		if not name:
-			name = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-
-		self.logDir = "%s/%s" % (DEF_TEST_OUT_DIR, name)
-		if os.path.exists(self.logDir):
-			self.debug.error("'%s' already exists!" % self.logDir)
-			return False
-
-		self.debug.dbg("logDir: %s" % self.logDir)
-		os.system("mkdir -p %s" % self.logDir)
-
-		# 保存测试配置及命令，以免测试数据对不上
-		os.system("cp -f %s %s/EMUL_CONFIG" % (self.testCfg, self.logDir))
-		os.system("echo %s > %s/WIN_CMD" % (" ".join(argv), self.logDir))
-		return True
-
-	def start (self, argv, name = None):
+	def start (self, argv, name):
 		"""
 		模拟测试入口
 		:param argv: 命令参数列表
@@ -638,7 +541,7 @@ class Emulation:
 		:return: 成功返回True，否则返回False
 		"""
 		# 初始化测试环境
-		if not self.__initTestEnv(argv, name):
+		if not self.initTestEnv(argv, name):
 			return False
 
 		try:
