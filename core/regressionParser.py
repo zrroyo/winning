f63ebe32b07ce2320c7ed4e86dc2e9ec55255df5
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2017-06-03 21:03:29 +0800

    core:regressionParser: support the Independent mode
    Support the Independent mode to execute a set of contracts independently.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- core/regressionParser.py
+++ core/regressionParser.py
@@ -15,14 +15,17 @@ import time
 
 from misc.debug import Debug
 from misc.execcmd import ExecCommand
-from core.emulation import Emulation, ExceptionEmulUnknown, DEF_EMUL_CONFIG_DIR
+from core.tbase import ExceptionEmulUnknown, DEF_EMUL_CONFIG_DIR
+from core.emulation import Emulation
+from core.execution import Execution
 
 # 默认关闭debug信息
 debug = Debug('Regression', False)
 # shell命令执行接口
 shell = ExecCommand()
 
-def listTestConfigs ():
+
+def listTestConfigs():
 	"""
 	列出所有可选的测试配置文件
 	:return: None
@@ -32,10 +35,11 @@ def listTestConfigs ():
 	configs = '\n'.join(configs)
 	print configs
 
-def startRegression (strategy, test, argv, name,
-			dbgMode, storeLog):
+
+def startRegression(options, strategy, test, argv, name, dbgMode, storeLog):
 	"""
-	启动回归测试
+	启动测试
+	:param options:
 	:param strategy: 策略名
 	:param test: 测试配置名
 	:param argv: 命令参数列表
@@ -46,15 +50,28 @@ def startRegression (strategy, test, argv, name,
 	"""
 	try:
 		_start = time.time()
-		emulate = Emulation(cfg = test,
-				strategy = strategy,
-				debug = dbgMode,
-				storeLog = storeLog)
-		emulate.start(argv, name)
+
+		if options.emulation:
+			exc = Emulation(cfg = test,
+					strategy = strategy,
+					debug = dbgMode,
+					storeLog = storeLog)
+			exc.start(argv, name)
+		else:
+			exc = Execution(cfg = test,
+					strategy = strategy,
+					debug = dbgMode,
+					storeLog = storeLog)
+
+			if options.jobs:
+				exc.start(argv, name, jobs = int(options.jobs))
+			else:
+				exc.start(argv, name)
+
 		_end = time.time()
 		debug.info("Time Execution: %ss" % (_end - _start))
 		_start = _end
-		emulate.report()
+		exc.report()
 		_end = time.time()
 		debug.info("Time Report: %ss" % (_end - _start))
 		return True
@@ -62,7 +79,8 @@ def startRegression (strategy, test, argv, name,
 		debug.error("startRegression: error: %s!" % e)
 		return False
 
-def regressionOptionsHandler (options, argv):
+
+def regressionOptionsHandler(options, argv):
 	"""
 	回归测试命令解析函数
 	:param options: 选项集
@@ -85,10 +103,11 @@ def regressionOptionsHandler (options, argv):
 		debug.error("Please select a test configuration!")
 		return False
 
-	return startRegression(options.strategy, options.test, argv,
-				options.name, options.debug, options.storeLog)
+	return startRegression(options, options.strategy, options.test, argv,
+			options.name, options.debug, options.storeLog)
 
-def regressionOptionsParser (parser, argv):
+
+def regressionOptionsParser(parser, argv):
 	"""
 	回归测试命令解析入口
 	:param parser: OptionParser接口对象
@@ -102,6 +121,10 @@ def regressionOptionsParser (parser, argv):
 			help='Select a test configuration.')
 	parser.add_option('-n', '--name', dest='name',
 			help='Name for a regress test.')
+	parser.add_option('-e', '--emul', action="store_true", dest='emulation',
+			help='Run tests in emulation mode.')
+	parser.add_option('-j', '--jobs', dest='jobs',
+			help='Maximum test jobs allowed.')
 	parser.add_option('-S', '--storeLog', action="store_true", dest='storeLog',
 			help='Store logs for each contract.')
 	parser.add_option('-D', '--debug', action="store_true", dest='debug',
