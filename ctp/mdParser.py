e64af1dbfe790c1f8464aacaf6a21d928a1ce4e1
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2020-02-06 15:09:03 +0800

    w2: ctp/mdParser.py
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- ctp/mdParser.py
+++ ctp/mdParser.py
@@ -20,7 +20,6 @@ import multiprocessing as mp
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-import misc.debug
 import ctpagent
 import ctp.futconfig as futconfig
 import core.corecfg as corecfg
@@ -40,22 +39,31 @@ class MarketDataLauncher(object):
 
     """
     #
-    CTP_CFG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'config', 'real.ini')
+    CFG_FILE = os.path.join(os.environ.get('HOME'), '.winning', 'config')
 
     def __init__(self):
         #
-        self.gCfg = corecfg.GlobalConfig('~/.winning/config')
+        self.gCfg = corecfg.GlobalConfig(MarketDataLauncher.CFG_FILE)
+        self.logDir = os.path.join(self.gCfg.getLogDir(), 'market_data')
+        if not os.path.exists(self.logDir):
+            os.mkdir(self.logDir)
+        #
+        self.fCfg = futconfig.CtpConfig(MarketDataLauncher.CFG_FILE)
+        self.workDir = self.fCfg.getDataDir('MarketData')
+        if not os.path.exists(self.workDir):
+            os.mkdir(self.workDir)
         #
         self.logger = None
-        self.workDir = os.path.join(self.gCfg.getLogDir(), 'market_data')
         #
         self.wkMdp = {}
         #
-        self.__insToFps = {}
+        self.insToFps = {}
         #
         self.mainThrExit = False
-        self.workerGoExit = False
         self.daemonGoExit = False
+        #
+        self.agent = None
+        self.dispatchInProgress = False
 
     @classmethod
     def makeLogger(cls, name, level, filename, format=None):
@@ -73,25 +81,17 @@ class MarketDataLauncher(object):
         logger.addHandler(fh)
         return logger
 
-    def workerSigHandler(self, sig, frame):
-        """
-
-        :param sig:
-        :param frame:
-        """
-        self.logger.warn('Received signal %s' % sig)
-        self.workerGoExit = True
-
     def workerMsgRsp(self, fd):
         pass
 
-    def workerExit(self, status):
+    def __workerExit(self):
         """
 
         """
-        for k, v in self.__insToFps:
+        for k, v in self.insToFps:
             fp = v['fp']
-            fp.close()
+            if fp:
+                fp.close()
 
     def rtnDepthMarketDataRsp(self, depth_market_data):
         """
@@ -110,53 +110,53 @@ class MarketDataLauncher(object):
             # print(u'[%s]，[价：最新/%g，买/%g，卖/%g], [量：买/%d，卖/%d，总：%d], [最高/%d，最低/%d], 时间：%s' % (
             # 	dp.InstrumentID, dp.LastPrice, dp.BidPrice1, dp.AskPrice1, dp.BidVolume1, dp.AskVolume1,
             # 	dp.Volume, dp.HighestPrice, dp.LowestPrice, dp.UpdateTime))
-            fp = self.__insToFps[dp.InstrumentID]['fp']
+            fp = self.insToFps[dp.InstrumentID]['fp']
             fp.write(depth_market_data)
         except Exception, e:
             self.logger.debug(u'接收行情数据异常! : \n%s' % (traceback.format_exc(e)))
 
-    def mdWorker(self, instruments, fdw):
+    def mdWorker(self, instruments, fdw, wkdir):
         """
 
         :param instruments:
         :param fdw:
+        :param wkdir:
         :return:
         """
         ret = 0
-        self.logger = MarketDataLauncher.makeLogger('worker', logging.DEBUG, 'worker-%s' % os.getpid(),
+        self.logger = MarketDataLauncher.makeLogger('worker', logging.DEBUG,
+                        os.path.join(self.logDir, 'worker-%s' % os.getpid()),
                         '%(asctime)s:%(name)s:%(funcName)s:%(lineno)d: <%(levelname)s> %(message)s')
-        # #
-        # signal.signal(signal.SIGKILL, self.workerSigHandler)
-        # signal.signal(signal.SIGTERM, self.workerSigHandler)
 
-        # Create file to store each instrument's data.
-        self.__insToFps = dict()
-        for i in instruments:
-            try:
-                fp = open(os.path.join(self.workDir, i), 'a')
-                self.__insToFps[i] = {'fp': fp}
-            except IOError, e:
-                self.logger.error("Failed to open data file for %s, due to:\n%s" % (i, traceback.format_exc(e)))
-                ret = 1
-                # Need notify the control-thread.
-                return ret
+        try:
+            # Create file to store each instrument's data.
+            self.insToFps = dict()
+            for i in instruments:
+                _file = os.path.join(wkdir, i)
+                fp = open(_file, 'a')
+                self.insToFps[i] = {'fp': fp}
 
-        # Start market data agent to receive data.
-        cfg = futconfig.CtpConfig(MarketDataLauncher.CTP_CFG_FILE)
-        agent = ctpagent.MarketDataAgent(instruments, cfg.getBrokerid('MarketData'),
-                     cfg.getInvestor('MarketData'), cfg.getPasswd('MarketData'), cfg.getServer('MarketData'))
-        agent.init_init(self.logger, self.rtnDepthMarketDataRsp)
+            # Init agent and start receiving market data.
+            agent = ctpagent.MarketDataAgent(self.fCfg.getBrokerid('MarketData'),
+                         self.fCfg.getInvestor('MarketData'), self.fCfg.getPasswd('MarketData'),
+                         self.fCfg.getServer('MarketData'))
+            agent.init_init(self.logger, self.rtnDepthMarketDataRsp)
+            agent.subscribe(instruments)
+        except Exception, e:
+            self.logger.error("Failed to init agent: \n%s", traceback.format_exc(e))
+            self.__workerExit()
+            ret = 1
+            sys.exit(ret)
 
         while 1:
-            rs, ws, es = select.select([fdw], None, [fdw], timeout=0.5)
-            if self.workerMsgRsp(rs) != 0:
-                ret = 1
+            rs, ws, es = select.select([fdw], [], [fdw], 0.5)
+            ret = self.workerMsgRsp(rs)
+            if ret:
                 break
 
-            if self.workerGoExit:
-                break
-
-        self.workerExit(ret)
+        agent.release()
+        self.__workerExit()
+        sys.exit(ret)
 
     def mainSigHandler(self, sig, frame):
         """
@@ -198,36 +198,29 @@ class MarketDataLauncher(object):
     def setupConsole(self):
         pass
 
-    def launch(self, instruments, jobs):
+    def __dispatch(self, instruments, jobs, tradingDay):
         """
 
         :param instruments:
         :param jobs:
-        :return:
+        :param tradingDay:
+        :return: 0|1
         """
-        ppid = os.getpid()
-        pid = os.fork()
-        if pid:
-            print("ppid %s, child %s" % (ppid, pid))
-            # In parent's context.
-            ret = 0
-            try:
-                signal.signal(signal.SIGUSR1, self.mainSigHandler)
-                # Wait until receive child's signal.
-                while not self.mainThrExit:
-                    time.sleep(1)
-                    continue
-            except KeyboardInterrupt, e:
-                os.kill(pid, signal.SIGUSR1)
-                ret = 1
+        ret = 1
+        # Convert passed patterns to real instruments code.
+        instruments = self.buildInstruments(instruments)
+        if not instruments:
+            self.logger.error("Can not build invalid instruments, exit!")
             return ret
 
-        # In child's context.
-        # From now on, child will be using its own debug logger.
-        self.logger = MarketDataLauncher.makeLogger('daemon', logging.DEBUG, 'daemon-%s' % os.getpid(),
-                        '%(asctime)s:%(name)s:%(funcName)s:%(lineno)d: <%(levelname)s> %(message)s')
-
-        signal.signal(signal.SIGUSR1, self.daemonSigHandler)
+        try:
+            # Create a directory to store the received data.
+            workDir = os.path.join(self.workDir, tradingDay)
+            if not os.path.exists(workDir):
+                os.mkdir(workDir)
+        except OSError, e:
+            self.logger.error("Failed to create data directory:\n%s" % traceback.format_exc(e))
+            return ret
 
         #
         nr = len(instruments.keys()) / jobs + 1
@@ -236,43 +229,89 @@ class MarketDataLauncher(object):
             for k in instruments.keys()[i*nr:(i+1)*nr]:
                 _instruments += list(instruments[k])
             rd, wr = mp.Pipe()
-            _proc = mp.Process(target=self.mdWorker, args=(_instruments, wr))
+            _proc = mp.Process(target=self.mdWorker, args=(_instruments, wr, workDir))
             _proc.start()
             mdp = MdProc(_proc, _proc.pid)
             self.wkMdp[rd] = mdp
 
+        if len(self.wkMdp):
+            ret = 0
+        return ret
+
+    def __validSignal(self):
+        """
+
+        :return:
+        """
+        ret = None
+        if self.dispatchInProgress:
+            return ret
+
         try:
-            # Parent may exit now.
-            os.kill(ppid, signal.SIGUSR1)
-        except OSError, e:
-            self.logger.error('Parent %s seems not existing any more, start exiting!' % ppid)
-            self.stopWorkers()
-            sys.exit(1)
+            if not self.agent:
+                self.agent = ctpagent.MarketDataAgent(self.fCfg.getBrokerid('MarketData'),
+                             self.fCfg.getInvestor('MarketData'), self.fCfg.getPasswd('MarketData'),
+                             self.fCfg.getServer('MarketData'))
+                self.agent.init_init()
+            _ret = self.agent.getTradingDay()
+            if _ret != '19800101':
+                ret = _ret
+        except Exception, e:
+            self.logger.error("Error to determine the signal to dispatch: %s\n" % traceback.format_exc(e))
+
+        return ret
+
+    def launch(self, instruments, jobs):
+        """
+
+        :param instruments:
+        :param jobs:
+        :return:
+        """
+        ret = 0
+        # From now on, child will be using its own debug logger.
+        self.logger = MarketDataLauncher.makeLogger('daemon', logging.DEBUG,
+                        os.path.join(self.logDir, 'daemon-%s' % os.getpid()),
+                        '%(asctime)s:%(name)s:%(funcName)s:%(lineno)d: <%(levelname)s> %(message)s')
+
+        # Daemon will exit if received SIGUSR1 signal.
+        signal.signal(signal.SIGUSR1, self.daemonSigHandler)
+        # Notify the Main to exit.
+        ppid = os.getppid()
+        os.kill(ppid, signal.SIGUSR1)
 
         #
         fdc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         fdc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         fdc.bind(('127.0.0.1', 9900))
         fdc.listen(10)
-        # rlist.append(fdc)
 
         while 1:
-            # Have received the exit signal.
+            tradingDay = self.__validSignal()
             if self.daemonGoExit:
+                # Received the signal from user to exit daemon.
                 break
-            time.sleep(1)
-            rlist = self.wkMdp.keys()
-            rs, ws, es = select.select(rlist, None, rlist, timeout=0.5)
-            for r in rs:
-                if r == fdc:
-                    self.consoleHandler(r)
-                else:
-                    self.daemonMsgRsp(r, self.wkMdp[r])
+            elif tradingDay:
+                if self.__dispatch(instruments, jobs, tradingDay):
+                    self.logger.error("Failed to dispatch workers.")
+                    ret = 1
+                    break
+                self.dispatchInProgress = True
+            else:
+                time.sleep(1)
+                rfds = self.wkMdp.keys() + [fdc]
+                rs, ws, es = select.select(rfds, [], rfds, 0.5)
+                for r in rs:
+                    if r == fdc:
+                        self.consoleHandler(r)
+                    else:
+                        self.daemonMsgRsp(r, self.wkMdp[r])
 
-        # Daemon exit.
         fdc.close()
         self.stopWorkers()
-        sys.exit(1)
+        if self.agent:
+            self.agent.release()
+        sys.exit(ret)
 
     def __buildInstruments(self, instrument, pattern=None):
         """
@@ -306,14 +345,14 @@ class MarketDataLauncher(object):
 
         patterns = patterns.split(',')
         for p in patterns:
-            _instrument = re.search('[A-Z]{1,2}', p).group()
-            if re.search('^[A-Z]{1,2}$', p):
+            _instrument = re.search('[a-z]{1,2}', p).group()
+            if re.search('^[a-z]{1,2}$', p):
                 # i.e. 'FG'
                 _ret = self.__buildInstruments(p)
-            elif re.search('^[A-Z]{1,2}\d{4}$', p):
+            elif re.search('^[a-z]{1,2}\d{4}$', p):
                 # i.e. 'FG2001'
                 _ret = [p]
-            elif re.search('^[A-Z]{1,2}((\d{0,3}\*)|(\d{0,2}\*\d)|(\d{0,1}\*\d{2})|(\*\d{3}))$', p):
+            elif re.search('^[a-z]{1,2}((\d{0,3}\*)|(\d{0,2}\*\d)|(\d{0,1}\*\d{2})|(\*\d{3}))$', p):
                 # such as 'FG*', 'FG19*', 'FG1*01', 'FG*01', etc
                 _ret = self.__buildInstruments(_instrument, p)
             else:
@@ -333,18 +372,28 @@ class MarketDataLauncher(object):
         Handler for passed commands.
         :return: 0|1
         """
-        ret = 1
+        ret = 0
 
         if options.select:
-            instruments = self.buildInstruments(options.select)
-            if not instruments:
-                return ret
-
             jobs = 1
             if options.jobs:
                 jobs = int(options.jobs)
 
-            if self.launch(instruments, jobs):
+            child = os.fork()
+            if child == 0:
+                # In child's context, expect child would never return.
+                self.launch(options.select, jobs)
+
+            # In parent's context.
+            try:
+                signal.signal(signal.SIGUSR1, self.mainSigHandler)
+                # Wait until receive child's signal.
+                while not self.mainThrExit:
+                    time.sleep(1)
+                    continue
+            except KeyboardInterrupt, e:
+                os.kill(child, signal.SIGUSR1)
+                ret = 1
                 return ret
 
         if options.list:
@@ -353,7 +402,6 @@ class MarketDataLauncher(object):
         if options.console:
             self.setupConsole()
 
-        ret = 0
         return ret
 
 
